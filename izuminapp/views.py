aab0e3db90350ce4237311aae29b6687c6e9ad62
KanataIZUMIKAWA <s1f101900194@iniad.org>
2022-02-22 07:04:02 +0900

    Add] EarthMC APIの読み込み

--- izuminapp/views.py
+++ izuminapp/views.py
@@ -1,9 +1,17 @@
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+import requests
 # from .models import Hoge
 
 def root(request):
     return render(request, 'izuminapp/root.html')
 
 def inca(request):
-    return render(request, 'inca/inca.html')
+    nations = requests.get("https://earthmc-api.herokuapp.com/nations/Inca_Empire")
+    if nations.status_code == 200 :
+        nations_info = dict(nations.json())
+        print(nations_info["king"])
+        inca_info = {"nodata": False}
+    else :
+        inca_info = {"nodata": True}
+    return render(request, 'inca/inca.html', inca_info)
