5793f0976f935eaee459eee45667b4fbd3b8895b
KanataIZUMIKAWA <s1f101900194@iniad.org>
2022-02-23 07:21:43 +0900

    [Add] APIの取得するかどうかの切り替え機能の実装

--- izuminapp/views.py
+++ izuminapp/views.py
@@ -11,12 +11,17 @@ def root(request):
 
 def inca(request):
     inca_info = {}
-    nations = requests.get("https://earthmc-api.herokuapp.com/nations/Inca_Empire")
-    if (nations.status_code == 200) and ABLE_API :
-        nations_info = dict(nations.json())
-        inca_info.update(nations_info)
-        inca_info["clTitle"] = SAMPLE_TITLE
-        inca_info["clPlayers"] = SAMPLE_PLAYER
+    if ABLE_API :
+        nations = requests.get("https://earthmc-api.herokuapp.com/nations/Inca_Empire")
+        if (nations.status_code == 200) and ABLE_API :
+            nations_info = dict(nations.json())
+            inca_info.update(nations_info)
+            inca_info["clTitle"] = SAMPLE_TITLE
+            inca_info["clPlayers"] = SAMPLE_PLAYER
+        else :
+            inca_info["nodata"] = True
     else :
         inca_info["nodata"] = True
+
+
     return render(request, 'inca/inca.html', inca_info)
