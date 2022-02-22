f0b55602ca0aa38ea24c5ee9e269ce40e3acd1ec
KanataIZUMIKAWA <s1f101900194@iniad.org>
2022-02-22 12:59:39 +0900

    [Change] ファーストビューの写真の情報をviewsで管理できるように変更

--- izuminapp/views.py
+++ izuminapp/views.py
@@ -1,17 +1,21 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 import requests
 # from .models import Hoge
+
+SAMPLE_TITLE = ["サンプル画像1", "サンプル画像2"]
+SAMPLE_PLAYER = ["KANATA2000", "かなた"]
 
 def root(request):
     return render(request, 'izuminapp/root.html')
 
 def inca(request):
+    inca_info = {}
     nations = requests.get("https://earthmc-api.herokuapp.com/nations/Inca_Empire")
     if nations.status_code == 200 :
         nations_info = dict(nations.json())
-        print(nations_info["king"])
-        inca_info = {"nodata": False}
+        inca_info.update(nations_info)
+        inca_info["clTitle"] = SAMPLE_TITLE
+        inca_info["clPlayers"] = SAMPLE_PLAYER
     else :
-        inca_info = {"nodata": True}
+        inca_info["nodata"] = True
     return render(request, 'inca/inca.html', inca_info)
