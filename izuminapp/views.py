cc6745cb3826df8afd0bbbd67734ba6eb4a6c89b
KanataIZUMIKAWA <s1f101900194@iniad.org>
2022-02-23 07:08:44 +0900

    [Cash Clear]

--- izuminapp/views.py
+++ izuminapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 import requests
 # from .models import Hoge
 
+ABLE_API = False
 SAMPLE_TITLE = ["サンプル画像1", "サンプル画像2"]
 SAMPLE_PLAYER = ["KANATA2000", "かなた"]
 
@@ -11,7 +12,7 @@ def root(request):
 def inca(request):
     inca_info = {}
     nations = requests.get("https://earthmc-api.herokuapp.com/nations/Inca_Empire")
-    if nations.status_code == 200 :
+    if (nations.status_code == 200) and ABLE_API :
         nations_info = dict(nations.json())
         inca_info.update(nations_info)
         inca_info["clTitle"] = SAMPLE_TITLE
