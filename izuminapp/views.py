713a9605314e0f5a43aea38e9fe8ffa26aeb4e0f
KanataIZUMIKAWA <s1f101900194@iniad.org>
2022-02-23 11:05:08 +0900

    [Change] APIの仕様の変更によるコードの変更

--- izuminapp/views.py
+++ izuminapp/views.py
@@ -1,23 +1,27 @@
 from django.shortcuts import render
 import requests
-# from .models import Hoge
+from izuminapp.model import Oldjson, Player, Firstview
 
-ABLE_API = False
+ABLE_API = True
+API_URL = "https://earthmc-api.herokuapp.com/api/v1"
 SAMPLE_TITLE = ["サンプル画像1", "サンプル画像2"]
 SAMPLE_PLAYER = ["KANATA2000", "かなた"]
 
 def root(request):
+    inca_info = {}
+    # return render(request, 'inca/inca.html', inca_info)
     return render(request, 'izuminapp/root.html')
 
 def inca(request):
     inca_info = {}
+    inca_info["clTitle"] = SAMPLE_TITLE
+    inca_info["clPlayers"] = SAMPLE_PLAYER
     if ABLE_API :
-        nations = requests.get("https://earthmc-api.herokuapp.com/nations/Inca_Empire")
+        nations = requests.get(API_URL + "/nations/Inca_Empire")
         if (nations.status_code == 200) and ABLE_API :
             nations_info = dict(nations.json())
             inca_info.update(nations_info)
-            inca_info["clTitle"] = SAMPLE_TITLE
-            inca_info["clPlayers"] = SAMPLE_PLAYER
+            # print(Oldjson.objects.all().count())
         else :
             inca_info["nodata"] = True
     else :
