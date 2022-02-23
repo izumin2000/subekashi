d4c55be4f0cda341a955f6bddfd6165086c3bcf2
KanataIZUMIKAWA <s1f101900194@iniad.org>
2022-02-23 11:01:44 +0900

    [DB] Firstviewクラスの追加

--- izuminapp/model.py
+++ izuminapp/model.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 class Oldjson(models.Model) :
-    nations = models.CharField(default = "", max_length=1000)
+    nations = models.CharField(default = "", max_length = 1000)
 
 class Player(models.Model) :
-    name = models.CharField(default = "", max_length=100)
-    info = models.CharField(default = "", max_length=1000)
+    name = models.CharField(default = "", max_length = 100)
+    info = models.CharField(default = "", max_length = 1000)
     online = models.BooleanField(default = False)
+
+class Firstview(models.Model) :
+    title = models.CharField(default = "", max_length = 100)
+    player = models.CharField(default = "", max_length = 100)
