eeaef755326a31604d50d703cc5ed2269c3f7d17
KanataIZUMIKAWA <s1f101900194@iniad.org>
2022-02-28 00:37:01 +0900

    [DB] Player.rankの追加

--- izuminapp/model.py
+++ izuminapp/model.py
@@ -1,20 +1,21 @@
 from django.db import models
 
 class Player(models.Model) :
-    name = models.CharField(default = "", max_length = 100)
-    uuid = models.CharField(default = "", max_length = 100)
-    info = models.CharField(default = "", max_length = 1000)
+    name = models.CharField(default = "", max_length = 50)
+    uuid = models.CharField(default = "", max_length = 50)
+    info = models.CharField(default = "", max_length = 200)
+    rank = models.CharField(default = "国民", max_length = 20)
     online = models.BooleanField(default = False)
     primary = models.BooleanField(default = False)
     leave = models.BooleanField(default = False)
 
 class Firstview(models.Model) :
     image = models.ImageField(upload_to='', null = True, blank = True)
-    title = models.CharField(default = "", max_length = 100)
-    player = models.CharField(default = "", max_length = 100)
+    title = models.CharField(default = "", max_length = 20)
+    player = models.CharField(default = "", max_length = 20)
 
 # シングルトン
 class Siteinfo(models.Model) :
     date = models.DateField(null = True, blank = True)
     visit = models.IntegerField(default = 0)
-    nations = models.CharField(default = "", max_length = 1000)
+    nations = models.CharField(default = "", max_length = 10000)
