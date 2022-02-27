5b0188d2b8f64553f29c43a7eed5786e15d7f0b1
KanataIZUMIKAWA <s1f101900194@iniad.org>
2022-02-28 01:08:00 +0900

    [Add] "主要プレイヤー"コンテンツの追加

--- izuminapp/views.py
+++ izuminapp/views.py
@@ -1,9 +1,6 @@
-from cgitb import reset
-from sys import platlibdir
-from turtle import title
 from django.shortcuts import render
 from izuminapp.forms import FirstviewForm
-from izuminapp.model import Oldjson, Player, Firstview, Siteinfo
+from izuminapp.model import Player, Firstview, Siteinfo
 import requests
 import datetime
 import json
@@ -12,7 +9,9 @@ EMC_API_URL = "https://earthmc-api.herokuapp.com/api/v1"
 UUID_API_URL = "https://api.mojang.com/users/profiles/minecraft/"
 NUMBER_OF_FIRSTVIEWS = 5
 ERROR_JSON = '{"population":"error","area":"error","king":"error","capitalName":"error","skin":"error"}'
-primary = ["RyoK3", "KATA2000", "sakira1996", "raimu569", "hiroshi4872", "ramuate"]
+PRIMARIES = ["Ryo5Syo5", "RyoK3", "KANATA2000", "sakira1996", "hiroshi4872", "ramuate"]
+PRIMARY_RANKS = {"Ryo5Syo5":"国王", "RyoK3":"財務大臣", "KANATA2000":"メディア大臣", "sakira1996":"外交大臣 副国王", "hiroshi4872":"国土交通大臣", "ramuate":"法務大臣"}
+
 
 def root(request):
     return render(request, 'izuminapp/root.html')
@@ -20,6 +19,8 @@ def root(request):
 def inca(request):
     inca_info = {}
 
+    # シングルトンインスタンスの生成
+    newSiteinfo, _ = Siteinfo.objects.get_or_create(pk = 0, defaults = {"nations" : ERROR_JSON})
 
     # ファーストビューの処理
     firstviews = Firstview.objects.order_by('?')[:min(Firstview.objects.count(), NUMBER_OF_FIRSTVIEWS)]     # ランダムにNUMBER_OF_FIRSTVIEWS個取り出す
@@ -28,27 +29,20 @@ def inca(request):
     inca_info["clTitle"] = [d.get('title') for d in firstviews]
     inca_info["clPlayers"] = [d.get('player') for d in firstviews]
 
-
     # APIの処理
     try :
         nations_get = requests.get(EMC_API_URL + "/nations/Inca_Empire")
     except Exception :      # ProxyErrorなら
         # jsonアーカイブからのアーカイブを読み込み
         inca_info["ableAPI"] = False
-        newOldjson, _ = Oldjson.objects.get_or_create(pk = 0, defaults = {"nations" : ERROR_JSON})
-        inca_info.update(dict(json.loads(newOldjson.nations)))      # 辞書型の結合
+        inca_info.update(dict(json.loads(newSiteinfo.nations)))      # 辞書型の結合
 
     else :      #正常にAPIを取得できたら
         inca_info["ableAPI"] = True
-        if nations_get.status_code != 200 :
+        if nations_get.status_code == 200 :
             # nationデータの取得
             nations_json = dict(nations_get.json())
             nations_json["population"] = len(nations_json["residents"])     # 人口の取得
-
-            # スキンの取得
-            uuid_get = requests.get(UUID_API_URL + nations_json["king"])
-            if uuid_get.status_code == 200 :
-                nations_json["skin"] = dict(uuid_get.json())["id"]
             
             inca_info.update(nations_json)      # 辞書型の結合
 
@@ -56,10 +50,15 @@ def inca(request):
             online_get = requests.get(EMC_API_URL + "/online")
             for player in nations_json["residents"] :
                 newPlayer, _ = Player.objects.get_or_create(name = player, defaults = {"name" : player})
-                if player in primary :      # 大臣なら
+                if player in PRIMARIES :      # 大臣なら
                     newPlayer.primary = True
-                else :
-                    newPlayer.primary = False
+                    newPlayer.rank = PRIMARY_RANKS[player]
+
+                # UUIDの登録
+                if newPlayer.uuid == "" :
+                    uuid_get = requests.get(UUID_API_URL + player)
+                    if uuid_get.status_code == 200 :
+                        newPlayer.uuid = dict(uuid_get.json())["id"]
                 
                 # onlineカラムの切り替え
                 if online_get.status_code == 200 :
@@ -70,24 +69,17 @@ def inca(request):
                     else :
                         newPlayer.online = False
                 newPlayer.save()
+            inca_info["primaries"] = Player.objects.filter(primary = True)
 
             # jsonのアーカイブ
-            newOldjson, _ = Oldjson.objects.update_or_create(pk = 0, defaults = {"nations" : json.dumps(nations_json)})
-            if newOldjson.nations == ERROR_JSON :
-                newOldjson.nations = json.dumps(nations_json)
-                newOldjson.save()
+            if newSiteinfo.nations in ["", ERROR_JSON] :
+                newSiteinfo.nations = json.dumps(nations_json)
+                newSiteinfo.save()
 
         # jsonアーカイブからのアーカイブを読み込み
         else :      # EMCサーバー側の問題なら
             inca_info["ableAPI"] = False
-            newOldjson, _ = Oldjson.objects.get_or_create(pk = 0, defaults = {"nations" : ERROR_JSON})
-            inca_info.update(dict(json.loads(newOldjson.nations)))
-
-    # スキンを一度も読み込んでいなかった際の処理
-    if inca_info["skin"] != "error" :
-        inca_info["loadedSkin"] = True
-    else :
-        inca_info["loadedSkin"] = False
+            inca_info.update(dict(json.loads(newSiteinfo.nations)))      # 辞書型の結合
 
     return render(request, 'inca/inca.html', inca_info)
 
