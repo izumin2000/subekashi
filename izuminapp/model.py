9d1ebfdd6a2e74220b496ae4aabddcf9efbe384f
KanataIZUMIKAWA <s1f101900194@iniad.org>
2022-02-28 00:22:43 +0900

    [DB] Player.uuid, Player.leave, Siteinfo.nationsの追加Oldjsonクラスの削除

--- izuminapp/model.py
+++ izuminapp/model.py
@@ -1,20 +1,20 @@
-from json.tool import main
 from django.db import models
-
-class Oldjson(models.Model) :
-    nations = models.CharField(default = "", max_length = 1000)
 
 class Player(models.Model) :
     name = models.CharField(default = "", max_length = 100)
+    uuid = models.CharField(default = "", max_length = 100)
     info = models.CharField(default = "", max_length = 1000)
     online = models.BooleanField(default = False)
     primary = models.BooleanField(default = False)
+    leave = models.BooleanField(default = False)
 
 class Firstview(models.Model) :
     image = models.ImageField(upload_to='', null = True, blank = True)
     title = models.CharField(default = "", max_length = 100)
     player = models.CharField(default = "", max_length = 100)
 
+# シングルトン
 class Siteinfo(models.Model) :
     date = models.DateField(null = True, blank = True)
     visit = models.IntegerField(default = 0)
+    nations = models.CharField(default = "", max_length = 1000)
