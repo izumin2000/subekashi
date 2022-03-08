b67197c35c6cfdc6a170eb6d4ab1feae11840317
izumin2000 <kanata.muses.c.2010613@gmail.com>
2022-03-08 17:43:52 +0900

    [HOT FIX] PostgreSQL用の設定

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: python manage.py migrate izuminapp
 web: gunicorn izuminapp.wsgi
--- izuminapp/settings.py
+++ izuminapp/settings.py
@@ -77,7 +77,8 @@ WSGI_APPLICATION = 'izuminapp.wsgi.application'
 load_dotenv(find_dotenv())
 # {'NAME': 'pj_db', 'USER': 'root', 'PASSWORD': 'Class9Team5', 'HOST': 'localhost', 'PORT': '', 'CONN_MAX_AGE': 600, 'ENGINE': 'django.db.backends.mysql'}
 default = dj_database_url.config(conn_max_age=600)
-default['OPTIONS'] = {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'", 'charset': 'utf8mb4'}
+# default['OPTIONS'] = {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'", 'charset': 'utf8mb4'} #MySQL用
+# postgresだとmigrate実行前に $ pg_ctl start -D "C:/Program Files/PostgreSQL/14/data"
 DATABASES = {
     'default': default,
 }
