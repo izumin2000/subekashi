473a31ba5b8b66de095095a773a1ad62d4ba273d
izumin2000 <kanata.muses.c.2010613@gmail.com>
2022-03-13 01:36:55 +0900

    [Change] formのリファクタリング

--- izuminapp/forms.py
+++ izuminapp/forms.py
@@ -2,13 +2,15 @@ from distutils.log import info
 from django import forms
 
 class FirstviewForm(forms.Form) :
-    image = forms.CharField()
-    title = forms.CharField()
-    player = forms.CharField()
+    image = forms.CharField(label="ファイル名")
+    title = forms.CharField(label="タイトル")
+    player = forms.CharField(label="プレイヤー")
+    password = forms.CharField(label="パスワード")
 
 class PlayerForm(forms.Form) :
-    name = forms.CharField()
-    info = forms.CharField()
-    rank = forms.CharField()
-    primary = forms.BooleanField()
-    crime = forms.BooleanField()
+    name = forms.CharField(label="プレイヤー名")
+    rank = forms.CharField(label="身分")
+    primary = forms.BooleanField(label="大臣？")
+    crime = forms.BooleanField(label="指名手配犯？")
+    info = forms.CharField(label="説明")
+    password = forms.CharField(label="パスワード")
