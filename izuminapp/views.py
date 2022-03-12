a13140833cb1f6b142d479e5ed8d17166ac79d86
izumin2000 <kanata.muses.c.2010613@gmail.com>
2022-03-13 01:37:08 +0900

    [Change] views.pyのリファクタリング

--- izuminapp/views.py
+++ izuminapp/views.py
@@ -21,14 +21,14 @@ def inca(request):
     inca_info = {}
 
     # シングルトンインスタンスの生成
-    newNation, _ = Singleton.objects.get_or_create(name = "nations" , defaults = {"value" : ERROR_JSON})
+    insSingleton, _ = Singleton.objects.get_or_create(name = "nations" , defaults = {"value" : ERROR_JSON})
 
     # ファーストビューの処理
-    firstviews = Firstview.objects.order_by('?')[:min(Firstview.objects.count(), NUMBER_OF_FIRSTVIEWS)]     # ランダムにNUMBER_OF_FIRSTVIEWS個取り出す
-    firstviews = list(firstviews.values())
-    inca_info["images"] = [d.get('image') for d in firstviews]
-    inca_info["clTitle"] = [d.get('title') for d in firstviews]
-    inca_info["clPlayers"] = [d.get('player') for d in firstviews]
+    insFirstviews = Firstview.objects.order_by('?')[:min(Firstview.objects.count(), NUMBER_OF_FIRSTVIEWS)]     # ランダムにNUMBER_OF_FIRSTVIEWS個取り出す
+    insFirstviews = list(insFirstviews.values())
+    inca_info["images"] = [d.get('image') for d in insFirstviews]
+    inca_info["clTitle"] = [d.get('title') for d in insFirstviews]
+    inca_info["clPlayers"] = [d.get('player') for d in insFirstviews]
 
     # APIの処理
     try :
@@ -36,7 +36,7 @@ def inca(request):
     except :      # ProxyErrorなら
         # jsonアーカイブからのアーカイブを読み込み
         inca_info["ableAPI"] = False
-        inca_info.update(dict(json.loads(newNation.value)))      # 辞書型の結合
+        inca_info.update(dict(json.loads(insSingleton.value)))      # 辞書型の結合
 
     else :      #正常にAPIを取得できたら
         inca_info["ableAPI"] = True
@@ -46,7 +46,7 @@ def inca(request):
                 nations_json = dict(nations_get.json())
             except :
                 inca_info["ableAPI"] = False
-                inca_info.update(dict(json.loads(newNation.value)))      # 辞書型の結合
+                inca_info.update(dict(json.loads(insSingleton.value)))      # 辞書型の結合
             else :
                 nations_json["population"] = len(nations_json["residents"])     # 人口の取得
                 
@@ -55,20 +55,20 @@ def inca(request):
                 # 国民の登録
                 online_get = requests.get(EMC_API_URL + "/online")
                 for player in nations_json["residents"] :
-                    newPlayer, _ = Player.objects.get_or_create(name = player, defaults = {"name" : player})
+                    insPlayer, _ = Player.objects.get_or_create(name = player, defaults = {"name" : player})
                     if player in PRIMARIES.keys() :      # 大臣なら
-                        newPlayer.primary = True
-                        newPlayer.rank = PRIMARIES[player]
+                        insPlayer.primary = True
+                        insPlayer.rank = PRIMARIES[player]
                     else :
-                        newPlayer.primary = False
-                        newPlayer.rank = "国民"
+                        insPlayer.primary = False
+                        insPlayer.rank = "国民"
 
                     # UUIDの登録
-                    if newPlayer.uuid == "" :
+                    if insPlayer.uuid == "" :
                         uuid_get = requests.get(UUID_API_URL + player)
                         if uuid_get.status_code == 200 :
                             try :
-                                newPlayer.uuid = dict(uuid_get.json())["id"]
+                                insPlayer.uuid = dict(uuid_get.json())["id"]
                             except :
                                 inca_info["ableAPI"] = False
 
@@ -77,27 +77,28 @@ def inca(request):
                         online_json = online_get.json()
                         online_players = [d.get('name') for d in online_json]
                         if player in online_players :
-                            newPlayer.online = True
+                            insPlayer.online = True
                         else :
-                            newPlayer.online = False
-                    newPlayer.save()
+                            insPlayer.online = False
+                    insPlayer.save()
             
                 # 他の国に移住した国民の登録
                 immigrants = set(Player.objects.values_list('name', flat = True)) - set(nations_json["residents"])
                 for immigrant in immigrants :
                     immigrant_player = Player.objects.filter(name = immigrant)[0]
                     immigrant_player.leave = True
+                    immigrant_player.rank = "元国民"
                     immigrant_player.save()
 
                 # jsonのアーカイブ
-                if newNation.value in ["", ERROR_JSON] :
-                    newNation.value = json.dumps(nations_json)
-                    newNation.save()
+                if insSingleton.value in ["", ERROR_JSON] :
+                    insSingleton.value = json.dumps(nations_json)
+                    insSingleton.save()
 
         # jsonアーカイブからのアーカイブを読み込み
         else :      # EMCサーバー側の問題なら
             inca_info["ableAPI"] = False
-            inca_info.update(dict(json.loads(newNation.value)))      # 辞書型の結合
+            inca_info.update(dict(json.loads(insSingleton.value)))      # 辞書型の結合
 
     inca_info["primaries"] = Player.objects.filter(primary = True)
     return render(request, 'inca/inca.html', inca_info)
@@ -107,15 +108,19 @@ def firstview(request) :
     result = {}
 
     if request.method == 'POST':
-        newimage = request.POST["image"]
-        newtitle = request.POST["title"]
-        newPlayer = request.POST["player"]
-        newimage = "firstview/" + newimage + ".png"
-        insFirstview, _ = Firstview.objects.get_or_create(image = newimage, defaults = {"image" : newimage})
-        insFirstview.title = newtitle
-        insFirstview.player = newPlayer
-        insFirstview.save()
-        result["title"] = newtitle + "をアップロードしました"
+        image = request.POST.get("image")       # form.pyにおいてid冒頭のid_はidに含まない
+        title = request.POST.get("title")
+        insPlayer = request.POST.get("player")
+        password = request.POST.get("password")
+        if password == "incagold" :
+            image = "firstview/" + image + ".png"
+            insFirstview, _ = Firstview.objects.get_or_create(image = image, defaults = {"image" : image})
+            insFirstview.title = title
+            insFirstview.player = insPlayer
+            insFirstview.save()
+            result["title"] = title + "をアップロードしました"
+        else :
+            result["title"] = "パスワードが違います"
     
     result["images"] = Firstview.objects.all()
 
