0816d7642949812d4ed18e448aed8d272b678492
izumin2000 <kanata.muses.c.2010613@gmail.com>
2022-05-04 16:05:43 +0900

    [DB change] DBの破壊的な変更

--- izuminapp/admin.py
+++ izuminapp/admin.py
@@ -1,28 +1,90 @@
 from django.contrib import admin
-from izuminapp.model import Player, Firstview, Singleton, Analyze
+from izuminapp.model import Player, Citizen, Minister, Criminal, Gold, Tour, Town, Nation, Firstview, Analyze
 
 
-class PlayerAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "nickname", "rank", "online", "leave", "crime", "uuid", "info")
-    list_display_links = ("id", "name")
-    ordering = ("id", )
+class PlayerAdmin(admin.ModelAdmin) :
+    list_display = ["id", "name", "nickname", "nation", "town", "online", "goldsam"]
+    list_display_links = ["id", "name"]
+    ordering = ["id"]
+    def nation(self, obj) :
+        return obj.nation.name
+    def town(self, obj) :
+        return obj.town.name
 
-class FirstviewAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "title", "player", "display")
-    list_display_links = ("id", "name")
-    ordering = ("id", )
+class CitizenAdmin(admin.ModelAdmin) :
+    list_display = ["player", "iscitizen"]
+    list_display_links = ["player"]
+    ordering = ["player"]
+    def player(self, obj) :
+        return obj.player.name
 
-class SingletonAdmin(admin.ModelAdmin):
-    list_display  = ("name", "value")
-    list_display_links = ("name", )
+class MinisterAdmin(admin.ModelAdmin) :
+    list_display = ["citizen", "title", "isminister"]
+    list_display_links = ["citizen"]
+    ordering = ["citizen"]
+    def citizen(self, obj) :
+        return obj.citizen.player.name
 
-class AnalyzeAdmin(admin.ModelAdmin):
-    list_display  = ("date", "pv")
-    list_display_links = ("date", )
-    ordering = ("id", )
+class CriminalAdmin(admin.ModelAdmin) :
+    list_display = ["player", "info", "x", "z", "isunderground"]
+    list_display_links = ["player"]
+    ordering = ["player"]
+    def player(self, obj) :
+        return obj.player.name
 
+class GoldAdmin(admin.ModelAdmin) :
+    list_display = ["player", "date", "amount"]
+    list_display_links = ["player"]
+    ordering = ["player"]
+    def player(self, obj) :
+        return obj.player.name
 
+class TourAdmin(admin.ModelAdmin) :
+    list_display = ["nation", "info"]
+    list_display_links = ["nation"]
+    ordering = ["nation"]
+    def nation(self, obj) :
+        return obj.nation.name
+
+class TownAdmin(admin.ModelAdmin) :
+    list_display = ["name", "nickname", "population", "area", "x", "z", "info", "mayor"]
+    list_display_links = ["name", "nickname"]
+    ordering = ["name"]
+    def mayor(self, obj) :
+        return obj.mayor.name
+
+class NationAdmin(admin.ModelAdmin) :
+    list_display = ["name", "nickname", "population", "area", "capital", "x", "z", "info", "king"]
+    list_display_links = ["name", "nickname"]
+    ordering = ["name"]
+    def capital(self, obj) :
+        return obj.capital.name
+
+class FirstviewAdmin(admin.ModelAdmin) :
+    list_display = ["id", "name", "title", "display"]
+    list_display_links = ["id", "name"]
+    ordering = ["id"]
+
+class AnalyzeAdmin(admin.ModelAdmin) :
+    list_display = ["date", "pv"]
+    list_display_links = ["date"]
+    ordering = ["id"]
+
+# Citizen, Minister, Criminal, Gold, Tour, Town, Nation,
 admin.site.register(Player, PlayerAdmin)
+admin.site.register(Citizen, CitizenAdmin)
+admin.site.register(Minister, MinisterAdmin)
+admin.site.register(Criminal, CriminalAdmin)
+admin.site.register(Gold, GoldAdmin)
+admin.site.register(Tour, TourAdmin)
+admin.site.register(Town, TourAdmin)
+admin.site.register(Nation, NationAdmin)
 admin.site.register(Firstview, FirstviewAdmin)
-admin.site.register(Singleton, SingletonAdmin)
 admin.site.register(Analyze, AnalyzeAdmin)
+
+"""
+class Admin(admin.ModelAdmin) :
+    list_display = [""]
+    list_display_links = ["id"]
+    ordering = ["id"]
+"""
--- izuminapp/model.py
+++ izuminapp/model.py
@@ -1,27 +1,66 @@
-from fileinput import filename
 from django.db import models
 
 class Player(models.Model) :
     name = models.CharField(default = "", max_length = 50)
     nickname = models.CharField(default = "", max_length = 50)
     uuid = models.CharField(default = "", max_length = 50)
+    town = models.ForeignKey("Town", on_delete = models.DO_NOTHING, blank = True, null = True, related_name = "player_town")
+    nation = models.ForeignKey("Nation", on_delete = models.DO_NOTHING, blank = True, null = True, related_name = "player_nation")
     info = models.CharField(default = "", max_length = 200)
-    rank = models.CharField(default = "国民", max_length = 20)
     online = models.BooleanField(default = False)
-    primary = models.BooleanField(default = False)
-    leave = models.BooleanField(default = False)
-    crime = models.BooleanField(default = False)
+    goldsam = models.IntegerField(default = 0)
+
+class Citizen(models.Model) :
+    player = models.ForeignKey("Player", on_delete = models.DO_NOTHING, blank = True, null = True)
+    iscitizen = models.BooleanField(default = True)
+
+class Minister(models.Model) :
+    citizen = models.ForeignKey("Citizen", on_delete = models.DO_NOTHING, blank = True, null = True)
+    title = models.CharField(default = "大臣", max_length = 20)
+    isminister = models.BooleanField(default = True)
+
+class Criminal(models.Model) :
+    player = models.ForeignKey("Player", on_delete = models.CASCADE)
+    info = models.CharField(default = "", max_length = 20)
+    x = models.IntegerField(default = 0)
+    z = models.IntegerField(default = 0)
+    isunderground = models.BooleanField(default = True)
+
+class Gold(models.Model) :
+    player = models.ForeignKey("Player", on_delete = models.CASCADE)
+    date = models.DateField(blank = True, null = True)
+    amount = models.IntegerField(default = 0)
+
+class Tour(models.Model) :
+    nation = models.ForeignKey("Nation", on_delete = models.DO_NOTHING, blank = True, null = True)
+    info = models.CharField(default = "", max_length = 500)
+    
+
+class Town(models.Model) :
+    name = models.CharField(default = "", max_length = 50)
+    nickname = models.CharField(default = "", max_length = 50)
+    population = models.IntegerField(default = 0)
+    area = models.IntegerField(default = 0)
+    x = models.IntegerField(default = 0)
+    z = models.IntegerField(default = 0)
+    info = models.CharField(default = "", max_length = 200)
+    mayor = models.ForeignKey("Player", on_delete = models.DO_NOTHING, blank = True, null = True, related_name = "town_mayor")
+
+class Nation(models.Model) :
+    name = models.CharField(default = "", max_length = 50)
+    nickname = models.CharField(default = "", max_length = 50)
+    population = models.IntegerField(default = 0)
+    area = models.IntegerField(default = 0)
+    capital = models.ForeignKey("Town", on_delete = models.DO_NOTHING, blank = True, null = True)
+    x = models.IntegerField(default = 0)
+    z = models.IntegerField(default = 0)
+    info = models.CharField(default = "", max_length = 200)
+    king = models.ForeignKey("Player", on_delete = models.DO_NOTHING, blank = True, null = True, related_name = "nation_king")
 
 class Firstview(models.Model) :
     name = models.CharField(default = "", max_length = 100)
-    title = models.CharField(default = "", max_length = 20)
-    player = models.CharField(default = "", max_length = 20)
+    title = models.CharField(default = "無題", max_length = 20)
     display = models.BooleanField(default = True)
-
-# シングルトン
-class Singleton(models.Model) :
-    name = models.CharField(default = "", max_length = 20)
-    value = models.CharField(default = "", max_length = 10000)
 
 class Analyze(models.Model) :
     date = models.DateField(null = True, blank = True)
