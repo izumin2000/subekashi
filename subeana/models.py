3685c80db44d4fbae7c1aa9b4571d33ba9b79a04
izumin2000 <kanata.muses.c.2010613@gmail.com>
2022-06-01 13:44:18 +0900

    [DB] subeana DBの作成

--- subeana/models.py
+++ subeana/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Channel(models.Model) :
+    name = models.CharField(default = "", max_length = 50)
+    isexist = models.BooleanField(default = True)
+
+    def __str__(self):
+        return self.name
+
+class Song(models.Model) :
+    title = models.CharField(default = "", max_length = 50)
+    lyrics = models.CharField(default = "", max_length = 5000)
+    url = models.CharField(default = "", max_length = 50, blank = True, null = True)
+    channel = models.ForeignKey("Channel", on_delete = models.DO_NOTHING, blank = True, null = True, related_name = "song_channel")
+    imitate = models.ForeignKey("Song", on_delete = models.DO_NOTHING, blank = True, null = True, related_name = "song_imitate")
+    isexist = models.BooleanField(default = True)
+    
+    def __str__(self):
+        return self.title
