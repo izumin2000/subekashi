6c8c33b2dc99c956fa7be98d4be9f25c934896f5
izumin2000 <kanata.muses.c.2010613@gmail.com>
2022-07-31 02:07:59 +0900

    [Change] topの変更

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from xia import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.top, name = 'top'),
     path('subeana/', include('subeana.urls')),
     path('xia/', include('xia.urls')),
 ]
