5834fad156412f290e3790d3e2cf4b1735559a1b
izumin2000 <kanata.muses.c.2010613@gmail.com>
2022-08-04 08:02:16 +0900

    [DB change] Citizenの削除など

--- xia/model.py
+++ xia/model.py
@@ -1,22 +1,17 @@
 from django.db import models
-from cloudinary.models import CloudinaryField
 
 class Player(models.Model) :
     name = models.CharField(default = "", max_length = 50)
     nickname = models.CharField(default = "", max_length = 50)
     uuid = models.CharField(default = "", max_length = 50)
     town = models.CharField(default = "", max_length = 50)
-    nation = models.ForeignKey("Nation", on_delete = models.DO_NOTHING, blank = True, null = True, related_name = "player_nation")
+    nation = models.CharField(default = "", max_length = 50)
     info = models.CharField(default = "", max_length = 200)
     online = models.BooleanField(default = False)
     goldsam = models.IntegerField(default = 0)
 
-class Citizen(models.Model) :
-    player = models.ForeignKey("Player", on_delete = models.DO_NOTHING, blank = True, null = True)
-    iscitizen = models.BooleanField(default = True)
-
 class Minister(models.Model) :
-    citizen = models.ForeignKey("Citizen", on_delete = models.DO_NOTHING, blank = True, null = True)
+    player = models.ForeignKey("Player", on_delete = models.CASCADE, blank = True, null = True)
     title = models.CharField(default = "大臣", max_length = 20)
     isminister = models.BooleanField(default = True)
 
@@ -45,7 +40,7 @@ class Nation(models.Model) :
     capital = models.CharField(default = "", max_length = 50)
     x = models.IntegerField(default = 0)
     z = models.IntegerField(default = 0)
-    king = models.ForeignKey("Player", on_delete = models.DO_NOTHING, blank = True, null = True, related_name = "nation_king")
+    king = models.CharField(default = "", max_length = 50)
     istour = models.BooleanField(default = False)
 
 class Analyze(models.Model) :
