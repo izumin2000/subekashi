34fa677fbef50bf8d1493a15efe988c39c24f53b
izumin2000 <kanata.muses.c.2010613@gmail.com>
2022-08-04 08:05:32 +0900

    [Add] editplayerの追加

--- xia/forms.py
+++ xia/forms.py
@@ -4,9 +4,7 @@ from django import forms
 class PlayerForm(forms.Form) :
     name = forms.CharField(label="プレイヤー名")
     info = forms.CharField(label="説明", required=False, widget=forms.Textarea())
-    password = forms.CharField(label="パスワード")
 
 class MinisterForm(forms.Form) :
     name = forms.CharField(label="プレイヤー名")
     title = forms.CharField(label="身分")
-    password = forms.CharField(label="パスワード")
