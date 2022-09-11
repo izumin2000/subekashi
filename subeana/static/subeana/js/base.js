5e3e2e9f464ca3ded361fdf0c67e15164aa48284
izumin2000 <kanata.muses.c.2010613@gmail.com>
2022-09-12 02:35:44 +0900

    [Change] テンプレートとjsの一般化

---
+++ subeana/static/subeana/js/base.js
@@ -0,0 +1,57 @@
+// 漢字のみおおきく
+function kanji() {
+    tags = ['p', 'a', 'h1', 'h2'];
+    for (tag of tags) {
+        for (p of document.getElementsByTagName(tag)) {
+            if (p.className != "nokanji") {
+                if ((tag == 'p') || (tag == 'a')) {
+                    p.innerHTML = p.innerHTML.replace(/([\u4E00-\u9FFF])/gi,"<font class=kanjipa>$1</font>");
+                } else if (tag == 'h1') {
+                    p.innerHTML = p.innerHTML.replace(/([\u4E00-\u9FFF])/gi,"<font class=kanjih1>$1</font>");
+                } else if (tag == 'h2') {
+                    p.innerHTML = p.innerHTML.replace(/([\u4E00-\u9FFF])/gi,"<font class=kanjih2>$1</font>");
+                }
+            }
+        }
+    }
+}
+
+// メニューの切り替え
+function menu() {
+    var isMenu = document.getElementById("menuarticle").style.display
+    if (isMenu != "block") {
+        document.getElementById("mainarticle").style.display = "none";
+        document.getElementById("menuicon").innerHTML = "<i class='fas fa-times'></i>";
+        document.getElementById("menuarticle").style.display = "block";
+        document.getElementsByTagName("main")[0].style.background = "linear-gradient(180deg, #000, #000, 70vh, #005)";
+    } else {
+        document.getElementById("mainarticle").style.display = "block";
+        document.getElementById("menuicon").innerHTML = "<i class='fas 	fas fa-bars'></i>";
+        document.getElementById("menuarticle").style.display = "none";
+        document.getElementsByTagName("main")[0].style.background = ""
+    }
+    kanji();
+}
+
+// 存在しないページのtoastr
+function notfound(){
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-bottom-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "timeOut": "3000",
+        "extendedTimeOut": "0",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+    }
+
+    toastr.info("このページはまだ存在しません！")
+};
+
+window.onload = function() {
+    kanji();
+}
