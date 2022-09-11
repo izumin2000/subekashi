5ba2d11272df9f10c43cf64c5d48f11c5509088a
izumin2000 <kanata.muses.c.2010613@gmail.com>
2022-09-12 04:51:36 +0900

    [Add] Songへの最低限のレコード追加機能の追加

--- subeana/views.py
+++ subeana/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+from subeana.models import Song
 
 # Create your views here.
 def top(request):
     return render(request, 'subeana/top.html')
 
 def new(request) :
+    if request.method == "POST":
+        title = request.POST.get("title")
+        channel = request.POST.get("channel")
+        url = request.POST.get("url")
+        imitate = request.POST.get("imitate")
+        lyrics = request.POST.get("lyrics")
+
+        ins_song = Song.objects.create()
+        ins_song.title = title
+        # ins_song.channel = channel
+        ins_song.url = url
+        ins_song.lyrics = lyrics
+        ins_song.save()
     return render(request, 'subeana/new.html')
