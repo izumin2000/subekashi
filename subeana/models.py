606c19d2cf9a65778a955fd3ab6bbbe6b43aee63
izumin2000 <kanata.muses.c.2010613@gmail.com>
2022-09-16 23:47:23 +0900

    [DB] モデルの単純化

--- subeana/models.py
+++ subeana/models.py
@@ -1,7 +1,7 @@
 from statistics import mode
 from django.db import models
 
-# Create your models here.
+
 class Channel(models.Model) :
     name = models.CharField(default = "", max_length = 50)
     url = models.CharField(default = "", max_length = 50, blank = True, null = True)
@@ -12,17 +12,17 @@ class Channel(models.Model) :
 
 class Song(models.Model) :
     title = models.CharField(default = "", max_length = 100)
-    lyrics = models.CharField(default = "", max_length = 10000)
+    channel = models.CharField(default = "", max_length = 50)
     url = models.CharField(default = "", max_length = 50, blank = True, null = True)
-    channel = models.ForeignKey("Channel", on_delete = models.DO_NOTHING, blank = True, null = True, related_name = "song_channel")
-    imitate = models.ForeignKey("Song", on_delete = models.DO_NOTHING, blank = True, null = True, related_name = "song_imitate")
+    lyrics = models.CharField(default = "", max_length = 10000)
+    imitate = models.CharField(default = "", max_length = 1000)
 
     def __str__(self):
         return self.title
 
 
 class Word(models.Model) :
-    song = models.ForeignKey("Song", on_delete = models.DO_NOTHING, blank = True, null = True, related_name = "word_song")
+    song = models.OneToOneField("Song", on_delete = models.DO_NOTHING, blank = True, null = True, related_name = "word_song")
     title = models.CharField(default = "", max_length = 100)
     words = models.CharField(default = "", max_length = 10000)
 
