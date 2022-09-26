3743941fe03bb77ca06e5db2b9ff24ee2feb757e
izumin2000 <kanata.muses.c.2010613@gmail.com>
2022-09-26 16:53:23 +0900

    [Change] Xiaのページの一般化

--- xia/static/xia/js/base.js
+++ xia/static/xia/js/base.js
@@ -1,35 +1,18 @@
-// 漢字のみおおきく
-function kanji() {
-    tags = ['p', 'a', 'h1', 'h2'];
-    for (tag of tags) {
-        for (p of document.getElementsByTagName(tag)) {
-            if (p.className != "nokanji") {
-                if (['p', 'a'].includes(tag)) {
-                    p.innerHTML = p.innerHTML.replace(/([\u4E00-\u9FFF])/gi,"<font class=kanjipa>$1</font>");
-                } else {
-                    p.innerHTML = p.innerHTML.replace(/([\u4E00-\u9FFF])/gi,"<font class=kanjih>$1</font>");
-                }
-            }
-        }
-    }
-}
-
-
 // メニューの切り替え
 function menu() {
     var isMenu = document.getElementById("menuarticle").style.display
+    console.log(isMenu)
     if (isMenu != "block") {
         document.getElementById("mainarticle").style.display = "none";
         document.getElementById("menuicon").innerHTML = "<i class='fas fa-times'></i>";
         document.getElementById("menuarticle").style.display = "block";
-        document.getElementsByTagName("main")[0].style.background = "linear-gradient(180deg, #000, #000, 70vh, #005)";
+        document.getElementsByTagName("main")[0].style.background = "linear-gradient(90deg, #eee, #eee, calc(40vw + 160px), #BB002D)";
     } else {
         document.getElementById("mainarticle").style.display = "block";
         document.getElementById("menuicon").innerHTML = "<i class='fas 	fas fa-bars'></i>";
         document.getElementById("menuarticle").style.display = "none";
         document.getElementsByTagName("main")[0].style.background = ""
     }
-    kanji();
 }
 
 
@@ -53,11 +36,26 @@ function notfound(){
 };
 
 
-// 可変テキストエリア
-function autotextarea() {
-    let textarea = document.getElementById('lyrics');
-    let clientHeight = textarea.clientHeight - 20;
-    textarea.style.height = clientHeight + 'px';
-    let scrollHeight = textarea.scrollHeight - 20;
-    textarea.style.height = scrollHeight + 'px';
+// コピーのtoastr
+function copyCode(text) {
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-bottom-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "timeOut": "3000",
+        "extendedTimeOut": "0",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+    }
+
+    if (Boolean(navigator.clipboard)) {
+        navigator.clipboard.writeText(text);
+        toastr.success(text + " をコピーしました！")
+    } else {
+        toastr.warning("この機能はセキュリティの関係上、HTTPS環境上でしか動作しません。")
+    }
 }
