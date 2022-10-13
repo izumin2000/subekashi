815c1ce979504921850713150de15a84dd29abf1
izumin2000 <kanata.muses.c.2010613@gmail.com>
2022-10-13 12:19:06 +0900

    [Fix] 歌詞の登録機能のバグ修正

--- subeana/views.py
+++ subeana/views.py
@@ -3,7 +3,43 @@ from subeana.models import Song
 from rest_framework import viewsets
 from .serializer import SongSerializer
 from config.settings import BASE_DIR as BASE_DIRpath
-# Create your views here.
+import hashlib
+import requests
+
+# パスワード関連
+SHA256a = "5802ea2ddcf64db0efef04a2fa4b3a5b256d1b0f3d657031bd6a330ec54abefd"
+
+
+def get_API(url) :
+    try :
+        get = requests.get(url)
+    except :        # プロキシエラー等のエラーが発生したら
+        print("5xx Error")
+        return ""
+
+    if (get.status_code == 200) :
+        try :
+            get_dict = get.json()
+        except :        # JSON形式ではなかったら（メンテナンス等）
+            print("Not JSON Error", get.status_code)
+            return ""
+
+        if "error" in get_dict :     # dictのキーにerrorがあったら
+            print("Invalid path Error", get.status_code)
+            return ""
+        
+        if "message" in get_dict :     # dictのキーにerrorがあったら
+            print("404 on json API", get.status_code)
+            
+        else :      # 正常に取得できたら
+            print("OK", get.status_code)
+            return get_dict
+
+    else :      # エラーステータスコードを受け取ったら（HEROKU error等）
+        print("not 2xx", get.status_code)
+        return ""
+
+
 def top(request):
     return render(request, 'subeana/top.html')
 
@@ -12,27 +48,29 @@ def new(request) :
     dir = {}
 
     if request.method == "POST":
-        title = request.POST.get("title")
-        channel = request.POST.get("channel")
-        url = request.POST.get("url")
-        lyrics = request.POST.get("lyrics")
-        imitatenums = request.POST.get("imitatenums")
+        inp_title = request.POST.get("title")
+        inp_channel = request.POST.get("channel")
+        inp_url = request.POST.get("url")
+        inp_lyrics = request.POST.get("lyrics")
+        inp_imitatenums = request.POST.get("imitatenums")
 
-        if ("" in [title, channel, imitatenums]) :
+        if ("" in [inp_title, inp_channel, inp_imitatenums]) :
             return render(request, "subeana/error.html")
 
-        ins_song, _ = Song.objects.get_or_create(title = title, defaults = {title : title})
-        ins_song.title = title
-        ins_song.channel = channel
-        if url :
-            if url in "https://www.youtube.com/watch?v=" :
-                url = "https://youtu.be/" + url[32:44]
+        ins_song, _ = Song.objects.get_or_create(title = inp_title, defaults = {"title" : inp_title})
+        ins_song.title = inp_title
+        ins_song.channel = inp_channel
+        if inp_url :
+            if inp_url in "https://www.youtube.com/watch?v=" :
+                url = "https://youtu.be/" + inp_url[32:44]
                 ins_song.url = url
-        if lyrics :
-            ins_song.lyrics = lyrics
+            else :
+                ins_song.url = inp_url
+        if inp_lyrics :
+            ins_song.lyrics = inp_lyrics
 
         imitates = set()
-        for i in range(int(imitatenums)) :
+        for i in range(int(inp_imitatenums)) :
             imitate = request.POST.get(f"imitate{i + 1}")
             if imitate == "模倣曲模倣" :
                 imitate = request.POST.get(f"imitateimitate{i + 1}")
@@ -62,6 +100,7 @@ def error(request) :
 
 
 def dev(request) :
+
     return render(request, "subeana/dev.html")
 
 
