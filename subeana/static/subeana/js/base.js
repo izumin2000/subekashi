fe21aa55012f80e85ed13482983b061f67a10248
izumin2000 <kanata.muses.c.2010613@gmail.com>
2022-11-10 20:28:42 +0900

    [Add] makeformの説明の追加

--- subeana/static/subeana/js/base.js
+++ subeana/static/subeana/js/base.js
@@ -1,10 +1,10 @@
 // 漢字のみおおきく
 function kanji() {
-    tags = ['p', 'a', 'h1', 'h2'];
+    tags = ['p', 'a', 'h1', 'h2', 'li'];
     for (tag of tags) {
         for (p of document.getElementsByTagName(tag)) {
             if (p.className != "nokanji") {
-                if (['p', 'a'].includes(tag)) {
+                if (['p', 'a', "li"].includes(tag)) {
                     p.innerHTML = p.innerHTML.replace(/([\u4E00-\u9FFF])/gi,"<font class=kanjipa>$1</font>");
                 } else {
                     p.innerHTML = p.innerHTML.replace(/([\u4E00-\u9FFF])/gi,"<font class=kanjih>$1</font>");
