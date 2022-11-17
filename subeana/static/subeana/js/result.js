5263d3b24057d119a1481289d0fa6a19c44e5912
izumin2000 <kanata.muses.c.2010613@gmail.com>
2022-11-17 21:21:25 +0900

    [Add] goodな歌詞の保存機能の実装

--- subeana/static/subeana/js/result.js
+++ subeana/static/subeana/js/result.js
@@ -1,11 +1,11 @@
 async function good(basedir, id) {
     goodEle = document.getElementById(id + "good");
-    if (goodEle.className == "fas fa-thumbs-up") {
+    if (goodEle.className == "fas fa-thumbs-up checkgood") {
         isgood = false;
-        goodEle.className = "far fa-thumbs-up";
+        goodEle.className = "far fa-thumbs-up checkgood";
     } else {
         isgood = true;
-        goodEle.className = "fas fa-thumbs-up";
+        goodEle.className = "fas fa-thumbs-up checkgood";
     }
 
     res = await fetch(
@@ -48,4 +48,35 @@ async function bad(basedir, id) {
             )
         }
     );
+}
+
+function copygood() {
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-bottom-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "timeOut": "3000",
+        "extendedTimeOut": "0",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+    }
+
+    checkgoodEles = document.getElementsByClassName("checkgood");
+    copytext = ""
+    for (checkgoodEle of checkgoodEles) {
+        if (checkgoodEle.className == "fas fa-thumbs-up checkgood") {
+            copytext += checkgoodEle.parentElement.innerText + "\n";
+        }
+    }
+
+    if (Boolean(navigator.clipboard)) {
+        navigator.clipboard.writeText(copytext);
+        toastr.success(copytext + " をコピーしました！")
+    } else {
+        toastr.warning("この機能はセキュリティの関係上、HTTPS環境上でしか動作しません。")
+    }
 }
