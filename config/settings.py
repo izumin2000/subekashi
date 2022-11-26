45da7c4b4e125e173ca835392cab3097b7031528
izumin2000 <kanata.muses.c.2010613@gmail.com>
2022-11-26 16:56:25 +0900

    [Delete] 不要なライブラリの削除

--- config/settings.py
+++ config/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'config.urls'
