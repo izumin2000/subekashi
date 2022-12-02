004fe0435eed1aec4f25227d4393cda566a5e033
izumin2000 <kanata.muses.c.2010613@gmail.com>
2022-12-02 22:29:39 +0900

    [DB change] Channel DBの削除・Song.imitatedの追加・生成情報カラムの追加

--- subekashi/models.py
+++ subekashi/models.py
@@ -1,14 +1,6 @@
 from django.db import models
 
 
-class Channel(models.Model) :
-    name = models.CharField(default = "", max_length = 50)
-    url = models.CharField(default = "", max_length = 50, blank = True, null = True)
-
-    def __str__(self):
-        return self.name
-
-
 class Song(models.Model) :
     title = models.CharField(default = "", max_length = 100)
     channel = models.CharField(default = "", max_length = 50)
@@ -16,6 +8,7 @@ class Song(models.Model) :
     lyrics = models.CharField(default = "", max_length = 10000, blank = True, null = True)
     ruigo = models.CharField(default = "", max_length = 10000, blank = True, null = True)
     imitate = models.CharField(default = "", max_length = 1000)
+    imitated = models.CharField(default = "", max_length = 1000)
     isjoke = models.BooleanField(default = False)
     isjapanese = models.BooleanField(default = True)
 
@@ -26,6 +19,9 @@ class Song(models.Model) :
 class Ai(models.Model) :
     lyrics = models.CharField(default = "", max_length = 100)
     points = models.IntegerField(default = 0)
+    genetype = models.CharField(default = "", max_length = 100)
+    category = models.CharField(default = "", max_length = 100)
+    similar = models.CharField(default = "", max_length = 100)
     isgpt = models.BooleanField(default = False)
     
     def __str__(self):
