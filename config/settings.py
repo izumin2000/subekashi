1139e0189057db8837356b077d435c2c8f9c6871
izumin2000 <kanata.muses.c.2010613@gmail.com>
2022-12-04 02:06:49 +0900

    [Chnage] pythonanywhereにデプロイする為の設定

--- config/settings.py
+++ config/settings.py
@@ -76,20 +76,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+
+load_dotenv(find_dotenv())
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-load_dotenv(find_dotenv())
-# {'NAME': 'pj_db', 'USER': 'root', 'PASSWORD': 'Class9Team5', 'HOST': 'localhost', 'PORT': '', 'CONN_MAX_AGE': 600, 'ENGINE': 'django.db.backends.mysql'}
-default = dj_database_url.config(conn_max_age=600)
-# default['OPTIONS'] = {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'", 'charset': 'utf8mb4'} #MySQL用
-# postgresだとmigrate実行前に $ pg_ctl start -D "C:/Program Files/PostgreSQL/14/data"
 DATABASES = {
-    'default': default,
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
-
-SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -132,18 +131,3 @@ DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
-
-
-if DEBUG :
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-else :
-    XIA_DISCORD_URL = os.environ['XIA_DISCORD_URL']
-    SUBEKASHI_DISCORD_URL = os.environ['SUBEKASHI_DISCORD_URL']
-
-
-# DEBUG = True
