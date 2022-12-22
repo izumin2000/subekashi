4f360f3e52f6e284c5bca2e4d9a0fefc218a245b
izumin2000 <kanata.muses.c.2010613@gmail.com>
2022-12-22 17:27:55 +0900

    [Fix] DISCORD_URLの秘匿化

--- xia/emcget.py
+++ xia/emcget.py
@@ -1,9 +1,9 @@
 from time import sleep 
 import requests
-DISCORD_URL = "https://discord.com/api/webhooks/1044246836644425758/6yV-POi0_9DDcfpL8oAqz8PJfBM3g8Dx1gOpBMcCjrloA3sU06HRH2CtDfxkkBUU0UmD"
+from config.settings import XIA_DISCORD_URL
 
 while 1 : 
     res = requests.get("https://earthmc.net/map/aurora/standalone/dynmap_earth.json")
     if res.status_code != 200 :
-        requests.post(DISCORD_URL, data={'content': f"エラーが発生しました。以下詳細です。\n f```{res.text}```"})
-    sleep(6)
+        requests.post(XIA_DISCORD_URL, data={'content': f"@KANATA200\nエラーが発生しました。以下詳細です。\n f```{res.text}```"})
+    sleep(5)
