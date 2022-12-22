ac4928e979327fce3ac089c750a906be3d603124
izumin2000 <kanata.muses.c.2010613@gmail.com>
2022-12-22 17:06:39 +0900

    [Add] emcgetのエラー通知機能

--- xia/emcget.py
+++ xia/emcget.py
@@ -1,5 +1,9 @@
 from time import sleep 
 import requests
+DISCORD_URL = "https://discord.com/api/webhooks/1044246836644425758/6yV-POi0_9DDcfpL8oAqz8PJfBM3g8Dx1gOpBMcCjrloA3sU06HRH2CtDfxkkBUU0UmD"
+
 while 1 : 
     res = requests.get("https://earthmc.net/map/aurora/standalone/dynmap_earth.json")
-    sleep(5)
+    if res.status_code != 200 :
+        requests.post(DISCORD_URL, data={'content': f"エラーが発生しました。以下詳細です。\n f```{res.text}```"})
+    sleep(6)
