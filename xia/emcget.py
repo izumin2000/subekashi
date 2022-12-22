36e405bbe32ea5d599b2850299b4873f0f5bd727
izumin2000 <kanata.muses.c.2010613@gmail.com>
2022-12-22 21:15:58 +0900

    [Fix] emcgetの400エラー修正

--- xia/emcget.py
+++ xia/emcget.py
@@ -1,9 +1,25 @@
-from time import sleep 
+from time import sleep
 import requests
-from config.settings import XIA_DISCORD_URL
+from orissaconfig import XIA_DISCORD_URL
 
-while 1 : 
+isfirst = 1
+isok = 1
+# requests.post(XIA_DISCORD_URL, data={'content': f"Hello Orissa!"})
+
+while 1 :
     res = requests.get("https://earthmc.net/map/aurora/standalone/dynmap_earth.json")
     if res.status_code != 200 :
-        requests.post(XIA_DISCORD_URL, data={'content': f"@KANATA200\nエラーが発生しました。以下詳細です。\n f```{res.text}```"})
-    sleep(5)
+        if isok :
+            content = f"エラーが発生しました。以下詳細です。\r```" + res.text.replace("\n", "\r") + "\r...```"
+            postres = requests.post(XIA_DISCORD_URL, data={"content": content})
+            isok = 0
+        print("error")
+    else :
+        if isfirst :
+            content = f"GETした結果は以下の通りです。\r```" + res.text.replace("\n", "\r") + "\r...```"
+            postres = requests.post(XIA_DISCORD_URL, data={"content": content})
+            print(postres)
+            isfirst = 0
+            print("start")
+        print(res.json()["currentcount"])
+    sleep(4)
