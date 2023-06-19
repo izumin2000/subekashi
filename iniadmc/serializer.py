5f2904a99c9ff26b57b8d1764cb088c0ded59a98
izumin2000 <kanata.muses.c.2010613@gmail.com>
2023-06-20 00:39:42 +0900

    [Delete] iniadmcアプリとorissaアプリの削除

--- iniadmc/serializer.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import fields
-from rest_framework import serializers
-from .models import Wait
-
-class WaitSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Wait
-        fields = ('minutes',)
