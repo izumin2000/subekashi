72a4c53ce3113274849aec2ab4fc2cd1808584df
izumin2000 <kanata.muses.c.2010613@gmail.com>
2023-08-05 03:06:56 +0900

    [Change] filltimeとslashコマンドをcleanコマンドへ統合

--- subekashi/management/commands/clean.py
+++ subekashi/management/commands/clean.py
@@ -1,27 +1,46 @@
 from django.core.management.base import BaseCommand
 from subekashi.models import Song
+from django.utils import timezone
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        songs = Song.objects.all() 
-        songsSet = set()
-        for song in songs :
-            songsSet.add(song.id)
-            if song.imitated and song.imitated[0] == "," :
-                song.imitated = song.imitated[1:]
-                song.save()
-                print("delete conma", song.id, song.imitated)
+        songInsL = Song.objects.all() 
+        songInsSet = set()
+        for songIns in songInsL :
+            songInsSet.add(songIns.id)
+            if songIns.imitated and songIns.imitated[0] == "," :
+                songIns.imitated = songIns.imitated[1:]
+                songIns.save()
+                print("delete conma", songIns.id)
 
-        deletedSongsSet = set(range(songs.last().id)) - songsSet
+        deletedSongsSet = set(range(songInsL.last().id)) - songInsSet
 
-        for song in songs :
-            imitated = song.imitated
+        for songIns in songInsL :
+            isChanged = False                
+
+            # imitatedの削除
+            imitated = songIns.imitated
             imitatedSet = set(map(int, imitated.split(","))) if bool(imitated) else set()
             if len(imitatedSet & deletedSongsSet) :
                 imitatedL = list(imitatedSet - deletedSongsSet)
                 imitated = ",".join(list(map(str, imitatedL))) if imitatedL else ""
-                print("delete deleted imiteted song(s)", song.id, imitated)
-                song.imitated = imitated
-                song.save()
+                print("delete deleted imiteted song(s)", songIns.id)
+                songIns.imitated = imitated
+                isChanged = True
+
+            # posttimeの埋め込み
+            if songIns.posttime == None :
+                songIns.posttime = timezone.now()
+                print("fill time", songIns.id)
+                isChanged = True
+            
+            # スラッシュの置換
+            if "/" in songIns.title :
+                songIns.title = songIns.title.replace("/", "╱")
+                print("replace slash", songIns.id)
+                isChanged = True
+            
+            if isChanged :
+                songIns.save()
