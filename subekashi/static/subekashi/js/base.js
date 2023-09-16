90026d1f88051fe64f223cde4bf413dfc3ce8a88
izumin2000 <kanata.muses.c.2010613@gmail.com>
2023-09-17 02:07:56 +0900

    [Clean]

--- subekashi/static/subekashi/js/base.js
+++ subekashi/static/subekashi/js/base.js
@@ -1,15 +1,15 @@
 // 漢字のみ大きく
 function kanji() {
-    tags = ['p', 'a', 'h1', 'h2', 'li', 'texterea'];
+    tags = ['p', 'a', 'h1', 'h2', 'li', 'textarea', 'input', 'button'];
     for (tag of tags) {
         for (charEle of document.getElementsByTagName(tag)) {
             if ((!charEle.classList.contains('staticSize') && (!charEle.classList.contains('buttona')) && (charEle != null))) {
                 var computedStyle = window.getComputedStyle(charEle);
                 var fontSize = computedStyle.getPropertyValue('font-size');
-                var FontSize = parseFloat(fontSize)
+                var FontSize = parseFloat(fontSize);
                 var sizeUnit = fontSize.replace(FontSize.toString(), "");
-                var newStyle = `font-size: ${parseInt(FontSize * 1.5)}${sizeUnit}`
-                var newCharEle = charEle.innerHTML.replace(/([\u4E00-\u9FFF])/gi,`<span style="${newStyle}">$1</span>`)
+                var newStyle = `font-size: ${parseInt(FontSize * 1.5)}${sizeUnit}`;
+                var newCharEle = charEle.innerHTML.replace(/([\u4E00-\u9FFF])/gi,`<span style="${newStyle}">$1</span>`);
                 charEle.innerHTML = newCharEle
             }
         }
