ed82126787ee6072536148d940d807ee639ea358
izumin2000 <kanata.muses.c.2010613@gmail.com>
2023-09-23 19:10:47 +0900

    [Delete] 不必要なDev formの削除

--- subekashi/models.py
+++ subekashi/models.py
@@ -12,7 +12,8 @@ class Song(models.Model) :
     uploaddata = models.DateField(blank = True, null = True)
     isoriginal = models.BooleanField(default = False)
     isjoke = models.BooleanField(default = False)
-    isjapanese = models.BooleanField(default = True)
+    isdeleted = models.BooleanField(default = True)
+    isarchived = models.BooleanField(default = True)
     isdraft = models.BooleanField(default = False)
     isinst = models.BooleanField(default = False)
     issubeana= models.BooleanField(default = True)
@@ -21,6 +22,25 @@ class Song(models.Model) :
     def __str__(self):
         return self.title
 
+
+class Version(models.Model) :
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    title = models.CharField(default = "", max_length = 500)
+    channel = models.CharField(default = "", max_length = 500)
+    url = models.CharField(default = "", max_length = 500, blank = True, null = True)
+    lyrics = models.CharField(default = "", max_length = 10000, blank = True, null = True)
+    imitate = models.CharField(default = "", max_length = 1000, blank = True, null = True)
+    imitated = models.CharField(default = "", max_length = 1000, blank = True, null = True)
+    posttime = models.DateTimeField(blank = True, null = True)
+    uploaddata = models.DateField(blank = True, null = True)
+    isoriginal = models.BooleanField(default = False)
+    isjoke = models.BooleanField(default = False)
+    isdeleted = models.BooleanField(default = True)
+    isarchived = models.BooleanField(default = True)
+    isdraft = models.BooleanField(default = False)
+    isinst = models.BooleanField(default = False)
+    issubeana= models.BooleanField(default = True)
+    ip = models.CharField(default = "", max_length = 100)
 
 class Ai(models.Model) :
     lyrics = models.CharField(default = "", max_length = 100)
