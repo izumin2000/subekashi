c45293291878975bd676a1029fd1d24e1262234a
izumin2000 <kanata.muses.c.2010613@gmail.com>
2023-09-30 19:27:40 +0900

    [Delete] mycommand.pyのキャッシュの削除

--- subekashi/management/commands/mycommand.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-from subekashi.models import Song
-
-
-class Command(BaseCommand) :
-    def handle(self, *args, **options):
-        songRec = Song.objects.get(id = options["id"])
-        
