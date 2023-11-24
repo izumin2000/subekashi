942bc36ec8f5c41081ce63c8555482085f2826ee
izumin2000 <kanata.muses.c.2010613@gmail.com>
2023-11-25 00:14:24 +0900

    [DB Change] Channelテーブルの追加

--- subekashi/models.py
+++ subekashi/models.py
@@ -16,30 +16,28 @@ class Song(models.Model) :
     isarchived = models.BooleanField(default = True)
     isdraft = models.BooleanField(default = False)
     isinst = models.BooleanField(default = False)
-    issubeana= models.BooleanField(default = True)
+    issubeana = models.BooleanField(default = True)
     ip = models.CharField(default = "", max_length = 100)
 
-    def __str__(self):
+    def __str__(self) :
         return self.title
 
 
+class Channel(models.Model) :
+    name = models.CharField(default = "", max_length = 100)
+    ismain = models.BooleanField(default = True)
+    isnickname = models.BooleanField(default = True)
+    subs = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='main')
+
+    def __str__(self) :
+        return self.name
+    
+
 class Version(models.Model) :
     song = models.ForeignKey(Song, on_delete=models.CASCADE)
-    title = models.CharField(default = "", max_length = 500)
-    channel = models.CharField(default = "", max_length = 500)
-    url = models.CharField(default = "", max_length = 500, blank = True, null = True)
-    lyrics = models.CharField(default = "", max_length = 10000, blank = True, null = True)
-    imitate = models.CharField(default = "", max_length = 1000, blank = True, null = True)
-    imitated = models.CharField(default = "", max_length = 1000, blank = True, null = True)
-    posttime = models.DateTimeField(blank = True, null = True)
-    uploaddata = models.DateField(blank = True, null = True)
-    isoriginal = models.BooleanField(default = False)
-    isjoke = models.BooleanField(default = False)
-    isdeleted = models.BooleanField(default = True)
-    isarchived = models.BooleanField(default = True)
-    isdraft = models.BooleanField(default = False)
-    isinst = models.BooleanField(default = False)
-    issubeana= models.BooleanField(default = True)
+    before = models.CharField(default = "", max_length = 10000)
+    after = models.CharField(default = "", max_length = 10000)
+    editedtime = models.DateTimeField(blank = True, null = True)
     ip = models.CharField(default = "", max_length = 100)
 
 class Ai(models.Model) :
