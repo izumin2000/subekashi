899c1a44c94cfe79c5f5233753b921f7b1db0233
izumin2000 <kanata.muses.c.2010613@gmail.com>
2024-02-29 02:37:58 +0900

    [Change] AiViewSetのバリテーション

--- subekashi/views.py
+++ subekashi/views.py
@@ -504,6 +504,22 @@ class SongViewSet(viewsets.ReadOnlyModelViewSet):
 class AiViewSet(viewsets.ModelViewSet):
     queryset = Ai.objects.all()
     serializer_class = AiSerializer
+    
+    def create(self, request, *args, **kwargs):
+        raise serializers.ValidationError("メソッドCREATEは受け付けていません")
+    
+    def update(self, request, *args, **kwargs):
+        if set(request.data.keys()) - {'view', 'score'}:
+            raise serializers.ValidationError("フィールドscore以外の変更は受け付けていません")
+        
+        return super().update(request, *args, **kwargs)
+    
+    def partial_update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
+    
+    def destroy(self, request, *args, **kwargs):
+        raise serializers.ValidationError("メソッドDELETEは受け付けていません")
 
 
 class AdViewSet(viewsets.ModelViewSet):
