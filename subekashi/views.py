cbea23ebe617387031a1892e4f858746b9224b57
izumin2000 <kanata.muses.c.2010613@gmail.com>
2024-05-10 21:26:42 +0900

    [Add] graphの対応

--- subekashi/views.py
+++ subekashi/views.py
@@ -452,7 +452,7 @@ def research(request) :
 
 
 def special(request) :
-    images = ["default", "autumn", "cool", "rainbow", "spring", "summer", "winter", "Blues_r", "BuGn_r", "BuPu_r", "GnBu_r", "Greens_r", "OrRd_r", "Spectral_r"]
+    images = ["graph_spring", "lyrics_default", "lyrics_autumn", "lyrics_cool", "lyrics_rainbow", "lyrics_spring", "lyrics_summer", "lyrics_winter", "lyrics_Blues_r", "lyrics_BuGn_r", "lyrics_BuPu_r", "lyrics_GnBu_r", "lyrics_Greens_r", "lyrics_OrRd_r", "lyrics_Spectral_r"]
     detaD = {"images": images}
     return render(request, "subekashi/special.html", detaD)
 
