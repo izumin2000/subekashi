a374f91111931840aaf66c617cf7e366322e75d5
izumin2000 <kanata.muses.c.2010613@gmail.com>
2024-09-10 00:36:28 +0900

    [Add] BAN機能の追加

---
+++ subekashi/middleware/restrict_ip.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from config.settings import *
+
+class RestrictIPMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        ban_path = os.path.join(BASE_DIR, 'subekashi/static/subekashi/md/ban.md')
+        self.banL = []
+        if os.path.exists(ban_path):
+            with open(ban_path, 'r', encoding='utf-8') as file:
+                ban_md = file.read()
+                banL = ban_md.split("\n")
+                self.banL = banL
+
+    def __call__(self, request):
+        ip_address = request.META.get('REMOTE_ADDR')
+        
+        if ip_address in self.banL and request.method in ['POST', 'PUT']:
+            return render(request, 'subekashi/500.html', status=500)
+
+        response = self.get_response(request)
+        return response
