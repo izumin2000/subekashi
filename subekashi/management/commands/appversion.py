bffd56467b74f558c72e45add5c3616c0953fc52
izumin2000 <kanata.muses.c.2010613@gmail.com>
2024-09-11 21:44:37 +0900

    [Change] setlastのアップデート

---
+++ subekashi/management/commands/appversion.py
@@ -0,0 +1,28 @@
+from config.settings import BASE_DIR
+from django.core.management.base import BaseCommand
+from datetime import date
+import subprocess
+import os
+
+
+class Command(BaseCommand):
+    help = "最終更新日のアップデート"
+
+    def handle(self, *args, **options) :
+        today = date.today().strftime("%Y-%m-%d")
+        v = options['v']
+        commit_count = subprocess.check_output(['git', 'rev-list', '--count', 'main']).strip().decode('utf8')
+        sub_version = v if v else commit_count
+        version = f"{today} (ver.{sub_version})"
+        version_path = os.path.join(BASE_DIR, 'subekashi/constants/dynamic/version.py')
+        if os.path.exists(version_path):
+            file = open(version_path, 'w', encoding='utf-8')
+            file.write(f'VERSION = "{version}"')
+            file.close()
+            self.stdout.write(self.style.SUCCESS(f"バージョン：{version}"))
+        else :
+            self.stdout.write(self.style.ERROR("subekashi/constants/dynamic/にversion.pyが存在しません。"))
+        return
+    
+    def add_arguments(self, parser):
+        parser.add_argument('--v', required=False, type=str)
