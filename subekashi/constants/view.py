18ea1e4cd78e857009c73d6996e8684912776752
izumin2000 <kanata.muses.c.2010613@gmail.com>
2024-09-13 23:50:20 +0900

    [Change] aiのレイアウト

--- subekashi/constants/view.py
+++ subekashi/constants/view.py
@@ -1,5 +1,4 @@
 DEFAULT_DESCRIPTION = "全て歌詞の所為です。は界隈曲をまとめたサイトです。"
-SHA256a = "5802ea2ddcf64db0efef04a2fa4b3a5b256d1b0f3d657031bd6a330ec54abefd"
 INPUT_TEXTS = ["title", "channel", "lyrics", "url"]
 INPUT_SELECTS = ["category", "songrange", "jokerange"]
 INPUT_FLAGS = ["isdraft", "isoriginal", "isinst"]
