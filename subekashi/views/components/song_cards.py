3d541844a68e3c6b7782699b471a8349115c9ad9
izumin2000 <kanata.muses.c.2010613@gmail.com>
2024-10-21 23:56:32 +0900

    [Add] song_cardsびwarningの追加

--- subekashi/views/components/song_cards.py
+++ subekashi/views/components/song_cards.py
@@ -18,6 +18,14 @@ def song_cards(request):
     
     if page == 1:
         result.append(f"<p>{Song.objects.count()}件中{statistics['count']}件ヒットしました</p>")
+    
+    sort = query.get("sort")
+    print(sort)
+    if "view" in sort:
+        result.append("<p class='warning'><i class='warning fas fa-exclamation-triangle'></i>システムの都合上、再生回数が1回以上の曲を表示しています。</p>")
+    
+    if "like" in sort:
+        result.append("<p class='warning'><i class='warning fas fa-exclamation-triangle'></i>システムの都合上、高評価数が1回以上の曲を表示しています。</p>")
         
     for song in song_qs:
         result.append(render_to_string('subekashi/components/song_card.html', {'song': song}))
