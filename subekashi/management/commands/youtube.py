1e94ba1b625a45dd699f002b1e70c623eed7453f
izumin2000 <kanata.muses.c.2010613@gmail.com>
2024-10-27 19:18:41 +0900

    [Add] lib/youtubeの追加

--- subekashi/management/commands/youtube.py
+++ subekashi/management/commands/youtube.py
@@ -1,10 +1,7 @@
 from django.core.management.base import BaseCommand
-from config.settings import *
 from subekashi.lib.url import *
+from subekashi.lib.youtube import *
 from subekashi.models import Song
-from googleapiclient.discovery import build
-from datetime import datetime
-from time import sleep
 
 
 class Command(BaseCommand):
@@ -15,43 +12,13 @@ class Command(BaseCommand):
         video_ids = [format_yt_url(url, True) for url in urls if is_yt_url(url)]
         return video_ids
     
-    def get_youtube_api(self, video_id):
-        sleep(2)
-        yt_res = {}
-        
-        try:
-            youtube = build("youtube", "v3", developerKey=YOUTUBE_API_KEY)
-            
-            request = youtube.videos().list(
-                part="snippet,statistics",
-                id=video_id
-            )
-            response = request.execute()
-            
-            item = response["items"][0]
-            
-            statistics = item["statistics"]
-            yt_res["view"] = int(statistics["viewCount"])
-            if not "likeCount" in statistics:
-                yt_res["like"] = int(statistics["favoriteCount"])
-            else:
-                yt_res["like"] = int(statistics["likeCount"])
-            upload_time = item["snippet"]["publishedAt"]
-            upload_time = datetime.strptime(upload_time, "%Y-%m-%dT%H:%M:%SZ")
-            yt_res["upload_time"] = upload_time
-            
-        except Exception as e:
-            pass
-            
-        return yt_res
-    
     def get_best_youtube_view(self, songs):
         best_view = 0
         video_ids = self.get_youtube_links(songs)
         best_res = {}
 
         for video_id in video_ids:
-            yt_res = self.get_youtube_api(video_id)
+            yt_res = get_youtube_api(video_id)
             view = yt_res.get("view", 0)
             like = yt_res.get("like", 0)
             upload_time = yt_res.get("upload_time", None)
