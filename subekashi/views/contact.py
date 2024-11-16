d6d58ce8026c46bfbe172a3dd27a27d310ae17ea
izumin2000 <kanata.muses.c.2010613@gmail.com>
2024-11-16 18:10:33 +0900

    [Add] 返信一覧の実装

--- subekashi/views/contact.py
+++ subekashi/views/contact.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from subekashi.lib.discord import *
 from subekashi.lib.ip import get_ip
+from subekashi.models import Contact
 
 
 def contact(request):
     dataD = {
-        "metatitle" : "フィードバック",
+        "metatitle" : "お問い合わせ",
     }
+    
+    contact_qs = Contact.objects.all().order_by("-id")
+    dataD["contact_qs"] = contact_qs
     
     if request.method == "POST" :
         contact_type = request.POST.get("contact-type")
@@ -15,14 +19,14 @@ def contact(request):
             dataD["result"] = "入力必須項目を入力してください。"
             return render(request, 'subekashi/contact.html', dataD)
 
-        content = f"種類：{contact_type}\n\
+        contact = f"種類：{contact_type}\n\
         詳細：{detail}\n\
         IP：{get_ip(request)}"
-        is_ok = sendDiscord(CONTACT_DISCORD_URL, content)
+        is_ok = sendDiscord(CONTACT_DISCORD_URL, contact)
         if not is_ok:
             dataD["result"] = "内部エラーが発生しました。"
             return render(request, 'subekashi/contact.html', dataD)
         
         dataD["result"] = "ok"
-        
+
     return render(request, 'subekashi/contact.html', dataD)
