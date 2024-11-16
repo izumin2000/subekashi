479ff2796d07a6079d1ae539cb7403392bb92b39
izumin2000 <kanata.muses.c.2010613@gmail.com>
2024-11-16 17:35:46 +0900

    [Add] ContentAdminの追加

--- subekashi/admin.py
+++ subekashi/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from .models import Song
+from .models import *
 
 
 class SongAdmin(admin.ModelAdmin):
     fields = ["title", "channel", "url", "imitate", "imitated", "lyrics", "isoriginal", "isjoke", "issubeana", "isdeleted", "isinst", "isarchived", "isdraft"]
 
 admin.site.register(Song, SongAdmin)
+
+class ContentAdmin(admin.ModelAdmin):
+    fields = ["detail", "post_time", "answer"]
+    
+admin.site.register(Contact, ContentAdmin)
