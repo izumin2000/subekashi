b0577a0a7b36179b5c0cbc137ce1d8c405bd5fda
izumin2000 <kanata.muses.c.2010613@gmail.com>
2024-12-08 18:24:03 +0900

    [Add] SongAdminのfieldsの追加

--- subekashi/admin.py
+++ subekashi/admin.py
@@ -4,7 +4,7 @@ from .models import *
 
 
 class SongAdmin(admin.ModelAdmin):
-    fields = ["title", "channel", "url", "imitate", "imitated", "lyrics", "isoriginal", "isjoke", "issubeana", "isdeleted", "isinst", "isarchived", "isdraft"]
+    fields = ["title", "channel", "url", "imitate", "imitated", "lyrics", "view", "lile", "isoriginal", "isjoke", "issubeana", "isdeleted", "isinst", "isarchived", "isdraft"]
 
 admin.site.register(Song, SongAdmin)
 
