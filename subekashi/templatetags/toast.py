0e0ec6194a4c2b08cc054200a9e71c5395ac642a
izumin2000 <kanata.muses.c.2010613@gmail.com>
2025-01-12 23:45:46 +0900

    [Add] toastの実装

---
+++ subekashi/templatetags/toast.py
@@ -0,0 +1,21 @@
+from django import template
+from django.template.loader import render_to_string
+
+register = template.Library()
+
+@register.simple_tag
+def get_toast(icon, text):
+    if not text:
+        return
+    
+    ICON_DICT = {
+        "ok": "fas fa-check-square ok",
+        "warning": "fas fa-exclamation-triangle warning",
+        "error": "fas fa-times-circle error"
+    }
+    icon_class = ICON_DICT[icon]
+    context = {
+        "icon_class": icon_class,
+        "text": text
+    }
+    return render_to_string('subekashi/components/toast.html', context)
