3a01e9d9a0b9c0093282268465fc64dd404c19ca
izumin2000 <kanata.muses.c.2010613@gmail.com>
2025-02-20 02:26:23 +0900

    [Change] last_modifiedをcontext_processorsに

--- subekashi/lib/context_processors.py
+++ subekashi/lib/context_processors.py
@@ -1,8 +1,17 @@
 from django.urls import resolve
 from subekashi.constants.constants import *
+from subekashi.constants.dynamic.version import VERSION
 
 def context_processors(request):
+    try:
+        from subekashi.constants.dynamic.version import VERSION
+        version = VERSION
+    except:
+        version = CONST_ERROR
+        
     context = {
         "aside_pages": ASIDE_PAGES,
+        "version": version,
     }
+    
     return context
