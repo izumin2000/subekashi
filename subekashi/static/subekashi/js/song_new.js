8ccd36c321a7a3678c2da289e54b1f2e6efb8bbf
izumin2000 <kanata.muses.c.2010613@gmail.com>
2025-02-21 14:09:30 +0900

    [Add] new-form-autoのバリデーション

--- subekashi/static/subekashi/js/song_new.js
+++ subekashi/static/subekashi/js/song_new.js
@@ -1,6 +1,6 @@
 const TUTORIALS = {
     "new-form-auto": "YouTubeのリンクからタイトル・チャンネル名を自動で取得して登録するフォームです。既に登録してあるURLは登録できません。",
-    "youtube-url": "YouTubeのURLを入力してください。<br>短縮されていないURLや「後で見る」を含むプレイリストで再生中の動画のURLでも大丈夫です。<br>無断転載された動画のURLの記載はお控えください。",
+    "youtube-url": "YouTubeのURLを入力してください。<br>「後で見る」を含むプレイリスト内の動画のURLでも大丈夫です。<br>無断転載された動画のURLの記載はお控えください。",
     "is-original": "オリジナル模倣曲・フリースタイル模倣曲の場合はチェックをつけてください。<br>例）<a href='https://youtu.be/XkIKM80-Znc' target='_blank'>∴∴∴∴</a> <a href='https://youtu.be/zDUvoKUOviQ' target='_blank'>天秤にかけて</a>",
     "is-deleted": "アクセスしても視聴できない場合にチェックしてください。<br>限定公開の場合はチェックする必要はありません。",
     "is-joke": "ネタに走っている曲の場合にチェックしてください。<br>ネタ曲かどうかの判断は個人の判断に任せます。",
@@ -11,6 +11,12 @@ const TUTORIALS = {
     "channel": "チャンネル名やアーティスト名を入力してください。<br>複数のアーティストが関わっている場合はコンマ(,)で区切って入力してください。<br>複数の名義がある場合は、現在使われている名義・チャンネル名を入力してください。<span style='font-size: 10px'>将来的に名義に関する情報を入力できる機能を実装予定です。</span>",
 }
 
+var songJson;
+window.addEventListener('load', async function () {
+    songJson = await getJson("song");
+});
+
+
 function showTutorial(place) {
     const tutorial = TUTORIALS[place];
     showToast("info", tutorial);
@@ -19,8 +25,38 @@ function showTutorial(place) {
 // URL入力フォームの入力チェック
 const urlEle = document.getElementById('url');
 urlEle.addEventListener('input', function () {
-    // TODO そのurlがすでに登録されているかどうかのチェック
     const newSubmitAutoEle = document.getElementById('new-submit-auto');
+    const newFormAutoInfoEle = document.getElementById('new-form-auto-info');
+    const inputUrlEle = urlEle.value;
+    const videoId = getYouTubeVideoId(inputUrlEle);
+
+    // URLが空の場合
+    if (inputUrlEle === '') {
+        newFormAutoInfoEle.innerHTML = "";
+        newSubmitAutoEle.disabled = true;
+        return;
+    }
+
+    // URLがYouTubeのURLでない場合
+    if (videoId === null) {
+        newFormAutoInfoEle.innerHTML = "<span class='error'>YouTubeの動画URLを入力してください</span>";
+        newSubmitAutoEle.disabled = true;
+        return;
+    }
+
+    const existingSong = songJson.find(song => getYouTubeVideoId(song.url) === videoId);
+
+    // 既に登録されているURLの場合
+    if (existingSong) {
+        newFormAutoInfoEle.innerHTML = `<span class='error'>このURLは<br>タイトル：${existingSong.title}<br>チャンネル名：${existingSong.channel}<br>として<a href="${baseURL()}/songs/${existingSong.id}" target="_blank">既に登録されています</a></span>`;
+        newSubmitAutoEle.disabled = true;
+        return;
+    }
+
+    // 登録されていないURLの場合
+    newFormAutoInfoEle.innerHTML = "<span class='ok'>このURLは登録されていません</span>";
+    newSubmitAutoEle.disabled = false;
+
     newSubmitAutoEle.disabled = urlEle.value == '';
 });
 
