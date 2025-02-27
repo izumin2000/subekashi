2b476ea29d3a0ae6da0a427eb667df8f199d5b59
izumin2000 <kanata.muses.c.2010613@gmail.com>
2025-02-27 17:59:25 +0900

    [Add] 模倣のsong_objの更新

--- subekashi/views/song_edit.py
+++ subekashi/views/song_edit.py
@@ -22,11 +22,11 @@ def song_edit(request, song_id) :
     }
 
     if request.method == "POST":
-        title = request.POST.get("title", "")
-        channel = request.POST.get("channel", "")
-        url = request.POST.get("url", "")
-        imitates = request.POST.get("imitates", "")
-        lyrics = request.POST.get("lyrics", "")
+        title = request.POST.get("title")
+        channel = request.POST.get("channel")
+        url = request.POST.get("url")
+        imitates = request.POST.get("imitate")
+        lyrics = request.POST.get("lyrics")
         is_original = bool(request.POST.get("is-original"))
         is_deleted = bool(request.POST.get("is-deleted"))
         is_joke = bool(request.POST.get("is-joke"))
@@ -38,91 +38,74 @@ def song_edit(request, song_id) :
         cleaned_url = clean_url(url)
         cleaned_url_list = cleaned_url.split(",")
         for cleaned_url_item in cleaned_url_list:
-            song_qs, _ = song_search({"url": cleaned_url_item})
-            if song_qs.exists() and url:
+            existing_song, _ = song_search({"url": cleaned_url_item})
+            if url and existing_song.exists() and existing_song.first().id != song_id :
                 return render(request, "subekashi/500.html", status=500)
-        
+
         # タイトルとチャンネルが空の場合はエラー
         if ("" in [title, channel]) :
             return render(request, "subekashi/500.html", status=500)
         
-        # 模倣関係以外のsong_objの更新
+        # DB保存用に変数を用意
         ip = get_ip(request)
         cleand_channel = channel.replace("/", "╱")
-        song_obj.update(
-            title = title,
-            channel = cleand_channel,
-            url = cleaned_url,
-            post_time = timezone.now(),
-            isoriginal = is_original,
-            isdeleted = is_deleted,
-            isjoke = is_joke,
-            isinst = is_inst,
-            issubeana = is_subeana,
-            ip = ip
-        )
+        cleand_lyrics = lyrics.replace("\r\n", "\n")
 
-        
-
-        
-        old_imitate_list = song_obj.imitate.split(",")
-        oldImitateS = set(old_imitate_list) - set([''])
-        newImitateS = set(imitates.split(",")) - set([''])
-
-        appendImitateS = newImitateS - oldImitateS
-        deleteImitateS = oldImitateS - newImitateS
-
-        for imitateId in appendImitateS :
-            imitatedIns = Song.objects.get(pk = imitateId)
-            imitatedInsL = set(imitatedIns.imitated.split(","))
-            imitatedInsL.add(str(songIns.id))
-            imitatedIns.imitated = ",".join(imitatedInsL)
-            imitatedIns.post_time = timezone.now()
-            imitatedIns.save()
-        for imitateId in deleteImitateS :
-            imitatedIns = Song.objects.get(pk = imitateId)
-            imitatedInsL = set(imitatedIns.imitated.split(","))
-            imitatedInsL.remove(str(songIns.id))
-            imitatedIns.imitated = ",".join(imitatedInsL)
-            imitatedIns.post_time = timezone.now()
-            imitatedIns.save()
-        songIns.imitate = imitates
-
-        songIns.lyrics = lyrics.replace("\r\n", "\n")
-        # TODO urlがURL_ICONにあるかのセキュリティチェック
-        urls = clean_url(url)
-        songIns.url = urls
-        songIns.isoriginal = int(bool(is_original))
-        songIns.isjoke = int(bool(is_joke))
-        songIns.isdeleted = int(bool(is_deleted))
-        songIns.isinst = int(bool(is_inst))
-        songIns.issubeana = int(bool(is_subeana))
-        songIns.isdraft = int(bool(is_draft))
-        songIns.post_time = timezone.now()
-        songIns.ip = ip
-        songIns.save()
-        
-                
+        # discordに通知
         content = f'\n\
-        {ROOT_URL}/songs/{id}\n\
+        編集されました\n\
+        {ROOT_URL}/songs/{song_id}\n\
         タイトル：{title}\n\
-        チャンネル : {channel}\n\
-        URL : {urls}\n\
-        ネタ曲 : {"Yes" if is_joke else "No"}\n\
-        IP : {ip}\n\
-        歌詞 : ```{lyrics}```'
-        is_ok = sendDiscord(NEW_DISCORD_URL, content)
-        if not is_ok:
-            return render(request, 'subekashi/500.html', status=500)
+        チャンネル : {cleand_channel}\n\
+        URL : {cleaned_url}\n\
+        ネタ曲 : {"Yes" if is_joke else "False"}\n\
+        すべあな模倣曲 : {"Yes" if is_subeana else "False"}\n\
+        IP : {ip}```'
+        # is_ok = sendDiscord(NEW_DISCORD_URL, content)
+        # if not is_ok:
+        #     song_obj.delete()
+        #     return render(request, 'subekashi/500.html', status=500)
+
+        # 新しい模倣の追加
+        old_imitate_id_set = set(song_obj.imitate.split(",")) - set([""])       # 元々の各模倣のID
+        new_imitate_id_set = set(imitates.split(",")) - set([""])       # ユーザーが入力した各模倣のID
+
+        append_imitate_id_set = new_imitate_id_set - old_imitate_id_set     # 編集によって新しく追加された各模倣のID
+        delete_imitate_id_set = old_imitate_id_set - new_imitate_id_set     # 編集によって削除された各模倣のID
+
+        # 編集によって新しく追加された各模倣先の被模倣に編集した曲のsong_idを追加する
+        for append_imitate_id in append_imitate_id_set :
+            append_imitate = Song.objects.get(pk = append_imitate_id)
+            append_imitated_id_set = set(append_imitate.imitated.split(","))        # 模倣先の被模倣
+            append_imitated_id_set.add(str(song_id))        # 模倣先の被模倣に編集した曲のsong_idを追加する
+            
+            append_imitate.imitated = ",".join(append_imitated_id_set)
+            append_imitate.save()
+            
+        # 編集によって削除された各模倣先の被模倣に編集した曲のsong_idを削除する
+        for delete_imitate_id in delete_imitate_id_set :
+            delete_imitate = Song.objects.get(pk = delete_imitate_id)
+            delete_imitated_id_set = set(delete_imitate.imitated.split(","))        # 模倣先の被模倣
+            delete_imitated_id_set.remove(str(song_id))   #   被模倣に編集した曲のsong_idを削除する
+                        
+            delete_imitate.imitated = ",".join(delete_imitated_id_set)
+            delete_imitate.save()
+
+        # song_objの更新
+        song_obj.title = title,
+        song_obj.channel = cleand_channel,
+        song_obj.url = cleaned_url,
+        song_obj.lyrics = cleand_lyrics,
+        song_obj.imitate = imitates,
+        song_obj.isoriginal = is_original,
+        song_obj.isdeleted = is_deleted,
+        song_obj.isjoke = is_joke,
+        song_obj.isinst = is_inst,
+        song_obj.issubeana = is_subeana,
+        song_obj.isdraft = is_draft,
+        song_obj.post_time = timezone.now()
+        song_obj.ip = ip
+        song_obj.save()
         
-        imitateInsL = []
-        if songIns.imitate :
-            imitateInsL = list(map(lambda i : Song.objects.get(pk = int(i)), songIns.imitate.split(",")))
-            dataD["imitateInsL"] = imitateInsL
-        dataD["songIns"] = songIns
-        dataD["channels"] = songIns.channel.replace(", ", ",").split(",")
-        dataD["urls"] = songIns.url.replace(", ", ",").split(",") if songIns.url else []
-        dataD["isExist"] = True
-        
-        return redirect(f'/songs/{songIns.id}')
+        return redirect(f'/songs/{song_id}')
     return render(request, 'subekashi/song_edit.html', dataD)
