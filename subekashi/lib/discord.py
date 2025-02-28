af131ec3b85c746dc49173f119fbe3a960984904
izumin2000 <kanata.muses.c.2010613@gmail.com>
2025-02-28 19:48:45 +0900

    [Add] sondDiscordのエラーハンドリング

--- subekashi/lib/discord.py
+++ subekashi/lib/discord.py
@@ -4,6 +4,10 @@ import requests
 
 # TODO send_discordへ
 def sendDiscord(url, content):
+    # urlが設定されていなかったら何もしない(コントリビュータ向け)
+    if not url:
+        return True
+    
     res = requests.post(url, data={'content': content})
     if (400 <= res.status_code < 600):
         return False
