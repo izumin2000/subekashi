a1b6592d7835e75f3401634fcdb0ee9974863d97
izumin2000 <kanata.muses.c.2010613@gmail.com>
2025-03-01 15:39:02 +0900

    [Clean] dupコマンドのリファクタリング

--- subekashi/management/commands/dup.py
+++ subekashi/management/commands/dup.py
@@ -1,46 +1,23 @@
 from django.core.management.base import BaseCommand
-from subekashi.models import *
-import re
+from subekashi.models import Song
+from subekashi.lib.url import *
 from collections import Counter
-
-
-URL_PATTERN = r'(?:\/|v=)([A-Za-z0-9_-]{11})(?:\?|&|$)'
-
-
-def re_yt_url(url):
-    match = re.search(URL_PATTERN, url)
-    return match    
-
-
-def is_yt_url(url):
-    match = re_yt_url(url)
-    return not match is None
-
-
-def format_yt_url(url, id=False):
-    match = re_yt_url(url)
-    if match is None:
-        return url
-    videoID = match.group(1)
-    if id:
-        return videoID
-    return "https://youtu.be/" + videoID
 
 class Command(BaseCommand) :
     def handle(self, *args, **options):
-        urls = Song.objects.values_list("url", flat=True)
-        ids = []
-        for url in urls :       # 各レコードのurlごとに
-            for one_url in url.split(",") :     # 1つのレコードの各urlごとに
-                if is_yt_url(one_url):      # URLがYouTubeなら
-                    id = format_yt_url(one_url, id=True)
-                    ids.append(id)
+        urls = sum([url.split(',') for url in Song.objects.values_list("url", flat=True)], [])       # Songに登録されカンマ区切りされた全URLのリスト
+        youtube_id_list = []    # YouTubeの動画リスト
+        for url in urls :
+            if is_yt_url(url):      # URLがYouTubeの動画URLなら
+                youtube_id = get_youtube_id(url)
+                youtube_id_list.append(youtube_id)
         
         is_dup = False
-        for id, count in Counter(ids).items():      # 
-            if count > 1:
+        for id, count in Counter(youtube_id_list).items():
+            if count > 1:       # 重複している動画IDがあったら
                 print(f"https://lyrics.imicomweb.com/search?url={id}")
                 is_dup = True
+                
         if not is_dup :
             print("重複は見つかりませんでした。")
 
