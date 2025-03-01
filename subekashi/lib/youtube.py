165cdef8ea7a3ce373681b847ed7173a2e9172cb
izumin2000 <kanata.muses.c.2010613@gmail.com>
2025-03-01 18:18:35 +0900

    [Add] 親の日時の取得 @ lib/youtube

--- subekashi/lib/youtube.py
+++ subekashi/lib/youtube.py
@@ -12,31 +12,39 @@ def get_youtube_api(video_id):
         # YouTubeからデータを取得
         youtube = build("youtube", "v3", developerKey=YOUTUBE_API_KEY)
         request = youtube.videos().list(
-            part="snippet,statistics",
+            part="snippet,statistics,liveStreamingDetails",
             id=video_id
         )
         response = request.execute()
         
         item = response["items"][0]
         statistics = item["statistics"]
+        snippet = item["snippet"]
         
+        # プレミア公開日時か投稿日時を取得
+        if "liveBroadcastContent" in snippet and snippet["liveBroadcastContent"] == "upcoming":
+            publish_time = snippet.get("scheduledPublishTime") or item.get("liveStreamingDetails", {}).get("scheduledStartTime")       # 親の日時
+        else:
+            publish_time = snippet["publishedAt"]       # 投稿日時
+
         # 日本標準時の投稿日時を取得
-        upload_time = datetime.strptime(item["snippet"]["publishedAt"], "%Y-%m-%dT%H:%M:%SZ")
+        upload_time_str = datetime.strptime(publish_time, "%Y-%m-%dT%H:%M:%SZ")
 
         utc_zone = pytz.utc
         jst_zone = pytz.timezone("Asia/Tokyo")
-        jst_upload_time = utc_zone.localize(upload_time).astimezone(jst_zone)
+        jst_upload_time = utc_zone.localize(upload_time_str).astimezone(jst_zone)
         
         # 返す辞書を作成
         yt_res = {
             "view": int(statistics["viewCount"]),
-            "like": int(statistics["likeCount" if ("likeCount" in statistics) else "favoriteCount"]),
+            "like": int(statistics.get("likeCount", statistics.get("favoriteCount", 0))),
             "title": item["snippet"]["title"],
             "channel": item["snippet"]["channelTitle"],
             "upload_time": jst_upload_time
         }
         
-    except Exception:
+    except Exception as e:
+        print(f'\033[31m{e}\033[0m')
         return {}
         
     return yt_res
