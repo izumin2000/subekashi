915d529468a85d80a42f51829d94e41660dd200f
izumin2000 <kanata.muses.c.2010613@gmail.com>
2025-03-01 14:45:31 +0900

    [Add] lib/urlのclean_urlのフォーマットの追加

--- subekashi/lib/url.py
+++ subekashi/lib/url.py
@@ -3,19 +3,17 @@ from urllib.parse import urlparse, urlunparse
 import re
 
 
-URL_PATTERN = r'(?:\/|v=)([A-Za-z0-9_-]{11})(?:\?|&|$)'
-
-
+# YouTubeの動画IDのパターンマッチ
 def re_yt_url(url):
-    match = re.search(URL_PATTERN, url)
+    match = re.search(r'(?:https?:\/\/)?(?:www\.|m\.)?(?:youtube\.com\/.*[?&]v=|youtu\.be\/)([a-zA-Z0-9_-]{11})', url)
     return match    
 
-
+# YouTubeの動画URLかどうか
 def is_yt_url(url):
     match = re_yt_url(url)
     return not match is None
 
-
+# TODO idごとに関数を分ける
 def format_yt_url(url, id=False):
     match = re_yt_url(url)
     if match is None:
@@ -25,13 +23,15 @@ def format_yt_url(url, id=False):
         return videoID
     return "https://youtu.be/" + videoID
 
+# XのURLのクエリを削除
 def format_x_url(url):
     parsed_url = urlparse(url)
     clean_url = urlunparse(parsed_url._replace(query='', fragment=''))
     return clean_url
 
-# TODO xのurlクエリを削除
+# URLを短縮しフォーマットする
 def clean_url(urls):
+    urls = urls.replace(" , ", ",").replace(" ,", ",").replace(", ", ",")
     urls = urls.replace("https://www.google.com/url?q=", "")
     urls = urls.replace("https://www.", "https://")
     urls = urls.replace("https://twitter.com", "https://x.com")
