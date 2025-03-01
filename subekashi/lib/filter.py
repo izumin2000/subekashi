85c3f47117ed88115c76a67260e672f776c894f5
izumin2000 <kanata.muses.c.2010613@gmail.com>
2025-03-01 19:41:48 +0900

    [Add] lib/filterにis_lackを作成

--- subekashi/lib/filter.py
+++ subekashi/lib/filter.py
@@ -1,6 +1,6 @@
 from django.db.models import Q
 
-
+# topやsearchにあるキーワード検索のフィルター
 def filter_by_keyword(keyword):
     return (
         Q(title__contains=keyword) |
@@ -8,7 +8,8 @@ def filter_by_keyword(keyword):
         Q(lyrics__contains=keyword) |
         Q(url__contains=keyword)
     )
-    
+
+# 模倣のフィルター
 def filter_by_imitate(imitate):
     imitate = str(imitate)
     return (
@@ -18,6 +19,7 @@ def filter_by_imitate(imitate):
         Q(imitate__contains=',' + imitate + ',')
     )
 
+# 被模倣のフィルター
 def filter_by_imitated(imitated):
     imitated = str(imitated)
     return (
@@ -27,14 +29,30 @@ def filter_by_imitated(imitated):
         Q(imitated__contains=',' + imitated + ',')
     )
 
+# 模倣曲の検索に利用するフィルター
 def filter_by_guesser(guesser):
     return (
         Q(title__contains=guesser) |
         Q(channel__contains=guesser)
     )
-    
+
+# 未完成フィルター
 filter_by_lack = (
     (Q(isdeleted=False) & Q(url="")) |
     (Q(isoriginal=False) & Q(issubeana=True) & Q(imitate="") & ~Q(channel="全てあなたの所為です。")) | 
     (Q(isinst=False) & Q(lyrics=""))
 )
+
+# 未完成かどうか
+def is_lack(song):
+    if (song.isdeleted == False) and (song.url == ""):
+        return True
+    
+    if (song.isoriginal == False) and (song.issubeana == False) and (song.imitate == "") and (song.channel == "全てあなたの所為です。"):
+        return True
+    
+    if (song.isinst == False) and (song.lyrics == ""):
+        return True
+    
+    return False   
+        
