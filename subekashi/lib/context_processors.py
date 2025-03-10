3883fa0691a336fb08b2bc106f71281d27c82924
izumin2000 <kanata.muses.c.2010613@gmail.com>
2025-03-10 14:00:26 +0900

    [Fix] バージョンがfooterに表示されないのでcontext_processorsを前のバージョンに

--- subekashi/lib/context_processors.py
+++ subekashi/lib/context_processors.py
@@ -1,8 +1,16 @@
-from subekashi.constants.constants import ASIDE_PAGES
+from django.urls import resolve
+from subekashi.constants.constants import *
 
 def context_processors(request):
+    try:
+        from subekashi.constants.dynamic.version import VERSION
+        version = VERSION
+    except:
+        version = CONST_ERROR
+        
     context = {
         "aside_pages": ASIDE_PAGES,
+        "version": version,
     }
     
     return context
