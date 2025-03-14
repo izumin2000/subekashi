4e629a6156f5d43985ab5e49af1d276dc1eeaaa7
izumin2000 <kanata.muses.c.2010613@gmail.com>
2025-03-15 00:24:21 +0900

    [Delete] 自動バージョンアップの削除

--- subekashi/urls.py
+++ subekashi/urls.py
@@ -33,7 +33,6 @@ urlpatterns = [
     path('favicon.ico', favicon, name='favicon'),
     path('.well-known/traffic-advice', trafficAdvice, name='traffic-advice'),
     path('api/',include(defaultRouter.urls)),
-    path('api/version', version_api),
     path('api/html/song_cards', song_cards, name='song_cards'),
     path('api/html/song_guessers', song_guessers, name='song_guessers'),
     path('api/html/toast', toast, name='toast'),
