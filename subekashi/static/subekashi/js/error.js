10eaa47b98f3108f4e86ab89a293e11fea26c1ea
izumin2000 <kanata.muses.c.2010613@gmail.com>
2025-05-04 15:40:47 +0900

    [Change] errorページのレイアウト

--- subekashi/static/subekashi/js/error.js
+++ subekashi/static/subekashi/js/error.js
@@ -1,14 +1,21 @@
 function autoScroll(element, speed, direction) {
-    let scrollTop = 0;
-    const interval = 5;
+    let scrollTop = element.scrollTop;
+    const interval = 4;
     const scrollDirection = direction === 'up' ? -1 : 1;
-  
-    function scroll() {
+
+    const timer = setInterval(() => {
         scrollTop += scrollDirection;
         element.scrollTop = scrollTop;
-    }
 
-    const scrollInterval = setInterval(scroll, speed * interval);
+        // 上方向：scrollTopが0以下で停止
+        if (scrollDirection === -1 && element.scrollTop <= 0) {
+            clearInterval(timer);
+        }
+        // 下方向：スクロールが末尾に到達したら停止
+        if (scrollDirection === 1 && element.scrollTop + element.clientHeight >= element.scrollHeight) {
+            clearInterval(timer);
+        }
+    }, speed * interval);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
