a9b72146ee39de9c00458be3fb9bef8e8c74555d
izumin2000 <kanata.muses.c.2010613@gmail.com>
2025-05-24 22:49:31 +0900

    [Change] ArticleAPIをReadOnlyに

--- subekashi/views/api/article.py
+++ subekashi/views/api/article.py
@@ -2,6 +2,6 @@ from subekashi.models import Article
 from rest_framework import viewsets
 from ...serializer import ArticleSerializer
 
-class ArticleAPI(viewsets.ModelViewSet):
+class ArticleAPI(viewsets.ReadOnlyModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
