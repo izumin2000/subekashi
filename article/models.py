142c6fbca33ccff5055794d3006170cfdaad96a0
izumin2000 <kanata.muses.c.2010613@gmail.com>
2025-06-03 21:37:50 +0900

    [DB Change] subekashi:Article -> article:Article

---
+++ article/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Article(models.Model) :
+    TAGS = (
+        ("news", "ニュース"),
+        ("release", "リリースノート"),
+        ("howto", "使い方"),
+        ("blog", "ブログ"),
+        ("other", "その他"),
+    )
+    article_id = models.CharField(default = "", max_length = 100, primary_key=True)
+    title = models.CharField(default = "", max_length = 500)
+    tag = models.CharField(default = "", choices=TAGS, max_length=10)
+    text = models.CharField(default = "", blank = True, null = True, max_length = 500)
+    post_time = models.DateTimeField(blank = True, null = True)
+    is_open = models.BooleanField(default = True)
+    is_md = models.BooleanField(default = True)
+
+    def __str__(self):
+        return self.title
