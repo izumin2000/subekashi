5e29487c42f51ad433d0ec941973dd3c1cb8ca09
izumin2000 <kanata.muses.c.2010613@gmail.com>
2025-06-08 00:24:59 +0900

    [DB Change] TAGSの要素とArticle.authorの追加

--- article/models.py
+++ article/models.py
@@ -6,10 +6,12 @@ class Article(models.Model) :
         ("release", "リリースノート"),
         ("howto", "使い方"),
         ("blog", "ブログ"),
+        ("tool", "ツール"),
         ("other", "その他"),
     )
     article_id = models.CharField(default = "", max_length = 100, primary_key=True)
     title = models.CharField(default = "", max_length = 500)
+    author = models.CharField(default = "", max_length = 50)
     tag = models.CharField(default = "", choices=TAGS, max_length=10)
     text = models.CharField(default = "", blank = True, null = True, max_length = 500)
     post_time = models.DateTimeField(blank = True, null = True)
