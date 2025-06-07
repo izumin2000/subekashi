5995dc7cec5a9a003ecb309463c79038fde4d281
izumin2000 <kanata.muses.c.2010613@gmail.com>
2025-06-08 00:41:39 +0900

    [Add] discordのcontentを全て送信する機能の追加

--- subekashi/lib/discord.py
+++ subekashi/lib/discord.py
@@ -1,20 +1,28 @@
-from config.settings import *
 import requests
+from time import sleep
 
 
 def send_discord(url, content):
-    # urlが設定されていなかったら何もしない(コントリビュータ向け)
+    # urlが設定されていなかったら何もしない
     if not url:
         return True
-    
+
+    # 余分なインデントの削除
     content = content.replace("\n            ", "\n")
     content = content.replace("\n        ", "\n")
-    
-    if len(content) > 2000:
-        content = content[:1997] + "..."
-    
-    res = requests.post(url, data={'content': content})
-    if (400 <= res.status_code < 600):
-        return False
-        
+
+    # メッセージを2000文字ごとに分割
+    CHUNC_SIZE = 2000
+    chunks = [content[i:i + CHUNC_SIZE] for i in range(0, len(content), CHUNC_SIZE)]
+
+    for i, chunk in enumerate(chunks):
+        # 送信
+        res = requests.post(url, data={'content': chunk})
+        if 400 <= res.status_code < 600:
+            return False
+
+        # 最後のチャンク以外は1秒待つ
+        if i < len(chunks) - 1:
+            sleep(1)
+
     return True
