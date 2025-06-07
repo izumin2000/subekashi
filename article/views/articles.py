a9be27bf59ce8611815378b4d350f7dd2278cd6a
izumin2000 <kanata.muses.c.2010613@gmail.com>
2025-06-08 00:25:03 +0900

    [Add] 記者の追加

--- article/views/articles.py
+++ article/views/articles.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from article.models import Article
 
 def articles(request):
-    dataD = {"articles": Article.objects.filter(is_open = True)}
+    dataD = {"articles": Article.objects.filter(is_open = True).order_by("-post_time")}
     return render(request, 'article/articles.html', dataD)
