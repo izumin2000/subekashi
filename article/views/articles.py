6a8358679a37e3163b895399189d1eb765eeb8f9
izumin2000 <kanata.muses.c.2010613@gmail.com>
2025-07-07 01:39:54 +0900

    [Add] articlesの検索機能

--- article/views/articles.py
+++ article/views/articles.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
-from django.views.decorators.cache import never_cache
+from django.db.models import Q
 from article.models import Article
 
-@never_cache
 def articles(request):
-    dataD = {"articles": Article.objects.filter(is_open = True).order_by("-post_time")}
-    return render(request, 'article/articles.html', dataD)
+    tag_filter = request.GET.get("tag", "")
+    keyword = request.GET.get("keyword", "")
+
+    articles = Article.objects.filter(is_open=True)
+
+    if tag_filter and tag_filter != "all":
+        articles = articles.filter(tag=tag_filter)
+
+    if keyword:
+        articles = articles.filter(
+            Q(title__icontains=keyword) |
+            Q(author__icontains=keyword) |
+            Q(text__icontains=keyword)
+        )
+
+    articles = articles.order_by("-post_time")
+
+    response = render(request, 'article/articles.html', {
+        "articles": articles,
+        "selected_tag": tag_filter,
+        "keyword": keyword,
+        "tags": Article.TAGS
+    })
+
+    return response
