d26beb22312be8fb96cb027be80267f8e2df2295
izumin2000 <kanata.muses.c.2010613@gmail.com>
2025-08-03 10:46:04 +0900

    [DB Change] Channel・ChannelLink・ChannelAliasの追加

--- subekashi/models.py
+++ subekashi/models.py
@@ -73,19 +73,47 @@ class SongLink(models.Model):
     song = models.ForeignKey(Song, on_delete = models.CASCADE, related_name="links")
 
 
-    def __str__(self) :
+# 曲の作者の情報
+class Channel(models.Model):
+    name = models.CharField(unique=True, max_length = 500)
+
+    def __str__(self):
         return self.name
     
-
-class Version(models.Model) :
-    song = models.ForeignKey(Song, on_delete=models.CASCADE)
-    before = models.CharField(default = "", max_length = 10000)
-    after = models.CharField(default = "", max_length = 10000)
-    editedtime = models.DateTimeField(blank = True, null = True)
-    ip = models.CharField(default = "", max_length = 100)
+    # def save(self, *args, **kwargs):
+        # super().save(*args, **kwargs)
 
 
-class Contact(models.Model) :
+# 曲の作者のwebページの情報
+class ChannelLink(models.Model):
+    url = models.CharField(max_length = 100)
+    channel = models.ForeignKey(Channel, on_delete = models.CASCADE, related_name="links")
+
+
+# 曲の作者の別の呼び方の情報
+# 曲の登録時や編集時に正式な呼び方に変更するために使用される
+class ChannelAlias(models.Model):
+    CHOICES = (
+        ("id", "ID"),
+        ("abbr", "略称"),
+        ("common", "通称"),
+        ("past", "以前の名称"),
+        ("sns", "SNSでの名称"),
+        ("spell", "表記揺れ"),
+        ("other", "別名義"),
+    )
+    
+    name = models.CharField(unique=True, max_length = 500)
+    alias_type = models.CharField(default = "other", choices=CHOICES, max_length=10)
+    channel = models.ForeignKey(Channel, on_delete = models.CASCADE, related_name="aliases")
+
+    def __str__(self):
+        return self.name
+    
+    # def save(self, *args, **kwargs):
+        # super().save(*args, **kwargs)
+
+
     detail = models.TextField(max_length = 10000)
     post_time = models.DateField()
     answer = models.TextField(blank = True, null = True, max_length = 10000)
