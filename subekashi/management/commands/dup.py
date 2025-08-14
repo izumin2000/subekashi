39fb17194185543422c473de3fcf503c563465ae
izumin2000 <kanata.muses.c.2010613@gmail.com>
2025-08-14 12:31:03 +0900

    [Change] dupコマンドをYoutube以外にも対応

--- subekashi/management/commands/dup.py
+++ subekashi/management/commands/dup.py
@@ -1,23 +1,23 @@
 from django.core.management.base import BaseCommand
+from config.settings import ROOT_URL
 from subekashi.models import Song
 from subekashi.lib.url import *
 from collections import Counter
 
 class Command(BaseCommand) :
     def handle(self, *args, **options):
-        urls = sum([url.split(',') for url in Song.objects.values_list("url", flat=True)], [])       # Songに登録されカンマ区切りされた全URLのリスト
-        youtube_id_list = []    # YouTubeの動画リスト
-        for url in urls :
-            if is_youtube_url(url):      # URLがYouTubeの動画URLなら
-                youtube_id = get_youtube_id(url)
-                youtube_id_list.append(youtube_id)
+        # Songに登録されカンマ区切りされた全URLのリスト
+        urls = sum([url.split(',') for url in Song.objects.values_list("url", flat=True)], [])
         
-        is_dup = False
-        for id, count in Counter(youtube_id_list).items():
-            if count > 1:       # 重複している動画IDがあったら
-                print(f"https://lyrics.imicomweb.com/songs/?url={id}/")
-                is_dup = True
+        is_all_unique = True
+        for url, count in Counter(urls).items():
+            # 重複しているURLが無かったらスキップ
+            if count <= 1:
+                continue
+            
+            print(f"{ROOT_URL}/songs/?url={url}")
+            is_all_unique = False
                 
-        if not is_dup :
-            print("重複は見つかりませんでした。")
+        if is_all_unique :
+            print("URLの重複は見つかりませんでした。")
 
