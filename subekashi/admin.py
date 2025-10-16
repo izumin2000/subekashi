c1b3cc6dffde015a7e30668f408c84a4a5c3e6f0
izumin2000 <kanata.muses.c.2010613@gmail.com>
2025-10-17 00:00:52 +0900

    [Clean] **Adminのfieldsを削除

--- subekashi/admin.py
+++ subekashi/admin.py
@@ -3,25 +3,18 @@ from .models import *
 
 
 class SongAdmin(admin.ModelAdmin):
-    fields = ["title", "channel", "url", "imitate", "imitated", "lyrics", "view", "like", "post_time", "upload_time", "isoriginal", "isjoke", "issubeana", "isdeleted", "isinst", "isdraft", "isspecial", "islock"]
+    pass
 
 admin.site.register(Song, SongAdmin)
 
 
 class ContentAdmin(admin.ModelAdmin):
-    fields = ["detail", "post_time", "answer"]
+    pass
     
 admin.site.register(Contact, ContentAdmin)
 
 
 class HistoryAdmin(admin.ModelAdmin):
-    fields = [field.name for field in History._meta.get_fields()].remove("id")
+    pass
     
 admin.site.register(History, HistoryAdmin)
-
-
-# TODO Unknown field(s) (songs, histories) specified for Editor. Check fields/fieldsets/exclude attributes of class EditorAdmin.エラーの修正
-# class EditorAdmin(admin.ModelAdmin):
-    # fields = [field.name for field in Editor._meta.get_fields()]
-    # 
-# admin.site.register(Editor, EditorAdmin)
