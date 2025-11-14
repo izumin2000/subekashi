a6e9a835c46fda1851deb9b53b94b7f87d006a16
izumin2000 <kanata.muses.c.2010613@gmail.com>
2025-11-14 09:50:17 +0900

    [Delete] 各シリアライザのupdateオーバライドメソッドの削除

--- subekashi/serializer.py
+++ subekashi/serializer.py
@@ -5,22 +5,15 @@ class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
         fields = '__all__'
-    
-    def update(self, instance, validated_data):
-        return super().update(instance, validated_data)
+
 
 class AiSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ai
         fields = '__all__'
-    
-    def update(self, instance, validated_data):
-        return super().update(instance, validated_data)
 
+    
 class AdSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ad
         fields = '__all__'
-    
-    def update(self, instance, validated_data):
-        return super().update(instance, validated_data)
