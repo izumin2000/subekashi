102c27b7e926aed0038d8cb69504c0392e5ddcc2
izumin2000 <kanata.muses.c.2010613@gmail.com>
2025-11-24 02:59:46 +0900

    [Delete] 「不正なパラメータが含まれています」エラーの修正 @ad

--- subekashi/views/ad.py
+++ subekashi/views/ad.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
-from subekashi.models import *
+from subekashi.models import Ad
 from subekashi.lib.url import *
-from subekashi.lib.security import *
 from subekashi.lib.discord import *
 from config.settings import DSP_DISCORD_URL
 
@@ -10,16 +9,12 @@ def ad(request) :
     dataD = {
         "metatitle" : "宣伝",
     }
-    check = ""
     urlForms = []
     for i in range(1, 4) :
         url = request.COOKIES.get(f"ad{i}") if request.COOKIES.get(f"ad{i}") else ""
         dataD[f"url{i}"] = url
         dataD[f"ad{i}"] = url
-        check += url
         urlForms.append(url)
-        
-    dataD["sha256"] = sha256(check)
     
     if request.method == "POST" :
         checkPOST = ""
@@ -28,25 +23,17 @@ def ad(request) :
         for i in range(1, 4) :
             urlForm = format_youtube_url(request.POST.get(f"url{i}", ""))
             adForm = format_youtube_url(request.POST.get(f"ad{i}", ""))
-            sha256Form = request.POST.get("sha256")
             
             checkPOST += adForm
             urlForms.append(urlForm)
             adForms.append(adForm)
-        
-        if sha256Form != sha256(checkPOST) :
-            dataD["error"] = "不正なパラメータが含まれています"
-            return render(request, "subekashi/ad.html", dataD)
         
         for i, urlForm, adForm in zip(range(1, 4), urlForms, adForms) :
             if urlForm == adForm :
                 continue
             
             adIns = Ad.objects.filter(url = adForm).first()
-            if (adIns == None) and (adForm != "") :
-                dataD["error"] = "内部エラーが発生しました"
-                return render(request, "subekashi/ad.html", dataD)
-            elif adForm != "" :
+            if adForm != "" :
                 adIns.dup -= 1
                 adIns.save()
             
@@ -58,7 +45,6 @@ def ad(request) :
                 dataD[f"ad{i}"] = ""
                 dataD[f"url{i}"] = ""
                 urlForms[i - 1] = ""
-                dataD["sha256"] = sha256("".join(urlForms))
                 return render(request, "subekashi/ad.html", dataD)
             
             adIns, isCreate = Ad.objects.get_or_create(url = urlForm)
