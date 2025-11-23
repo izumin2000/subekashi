9d6a0488284f5d0c00ba8de29ccfd75ffd340c31
izumin2000 <kanata.muses.c.2010613@gmail.com>
2025-11-24 01:16:46 +0900

    [Add] get_ipにraw引数を追加

--- subekashi/lib/ip.py
+++ subekashi/lib/ip.py
@@ -1,6 +1,6 @@
 from subekashi.lib.security import encrypt
 
-def get_ip(request):
+def get_ip(request, raw=False):
     forwarded_addresses = request.META.get('HTTP_X_FORWARDED_FOR')
     ip_address = forwarded_addresses.split(',')[0] if forwarded_addresses else request.META.get('REMOTE_ADDR')
-    return encrypt(ip_address)
+    return ip_address if raw else encrypt(ip_address)
