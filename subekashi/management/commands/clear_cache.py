aef84677c5b8d55566df1ec6cc2a48ae50b71007
izumin2000 <kanata.muses.c.2010613@gmail.com>
2025-12-12 22:09:41 +0900

    [Delete] clear_cacheコマンドの削除

--- subekashi/management/commands/clear_cache.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from django.core.cache import cache
-
-class Command(BaseCommand):
-    help = "Clear Django cache"
-
-    def handle(self, *args, **kwargs):
-        cache.clear()
-        self.stdout.write(self.style.SUCCESS("キャッシュクリアしました"))
