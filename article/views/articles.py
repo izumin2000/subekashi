3075146476c957715c169b43449cfef290697ff1
izumin2000 <kanata.muses.c.2010613@gmail.com>
2026-01-31 23:20:51 +0900

    [Add] 公開日が未来の記事は404にする

--- article/views/articles.py
+++ article/views/articles.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.db.models import Q
+from django.utils import timezone
 from article.models import Article
 
 def articles(request):
     tag_filter = request.GET.get("tag", "")
     keyword = request.GET.get("keyword", "")
 
-    articles = Article.objects.filter(is_open=True)
+    articles = Article.objects.filter(is_open=True, post_time__lte=timezone.now())
 
     if tag_filter and tag_filter != "all":
         articles = articles.filter(tag=tag_filter)
