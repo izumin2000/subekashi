58a4068727556d3cbff07d7caf4de0773331925b
izumin2000 <kanata.muses.c.2010613@gmail.com>
2026-08-05 20:21:47 +0900

    [Add] is_removedをfalseのままにするdeleteコマンドの引数の追加

--- subekashi/management/commands/delete.py
+++ subekashi/management/commands/delete.py
@@ -7,12 +7,18 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('ids', nargs='+', type=int)
+        parser.add_argument(
+            '-k', '--keep-links',
+            action='store_true',
+            help='指定した場合、song.links.update(is_removed=True) を実行しない',
+        )
 
     def handle(self, *args, **options):
         for song_id in options['ids']:
             try:
                 song = Song.objects.get(pk=song_id)
-                song.links.update(is_removed=True)
+                if not options['keep_links']:
+                    song.links.update(is_removed=True)
                 song.delete()
                 self.stdout.write(self.style.SUCCESS(f"{song_id}を削除しました"))
             except Song.DoesNotExist:
